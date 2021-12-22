90499970a72f5937e7034060635bec9736e2cc4b
Kim-Ju-Hyeon <wngus1310@naver.com>
2021-12-22 21:22:37 +0900

    1222

--- models/GTS/GTS_model_for_CNN_Project.py
+++ models/GTS/GTS_model_for_CNN_Project.py
@@ -1,10 +1,57 @@
 from models.GTS.gts_graph_learning import GTS_Graph_Learning
-from models.GTS.gts_forecasting_module import GTS_Spike_Decoding
-from utils.utils import build_dynamic_batch_edge_index
-
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from torch_geometric_temporal.nn.recurrent import DCRNN
+
+class GTS_Spike_Decoding(nn.Module):
+    def __init__(self, config):
+        super(GTS_Spike_Decoding, self).__init__()
+
+        self.config = config
+
+        self.num_nodes = config.nodes_num
+        self.nodes_feas = config.node_features
+
+        self.kernel_size = config.embedding.kernel_size
+        self.stride = config.embedding.stride
+        self.conv1_dim = config.embedding.conv_dim[0]
+        self.conv2_dim = config.embedding.conv_dim[1]
+
+        self.embedding_dim = config.embedding.embedding_dim
+
+        self.conv1 = torch.nn.Conv1d(self.nodes_feas, self.conv1_dim, self.kernel_size[0],
+                                     stride=self.stride[0])
+        self.conv2 = torch.nn.Conv1d(self.conv1_dim, self.conv2_dim,
+                                     self.kernel_size[1], stride=self.stride[1])
+
+        self.bn1 = torch.nn.BatchNorm1d(self.conv1_dim)
+        self.bn2 = torch.nn.BatchNorm1d(self.conv2_dim)
+
+        self.recurrent = DCRNN(self.embedding_dim, self.embedding_dim, self.config.diffusion_k)
+
+    def forward(self, x, edge_index, hidden_state):
+        batch_nodes = x.shape[0]
+        if len(x.shape) == 2:
+            x = x.reshape(batch_nodes, 1, -1)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.bn1(x)
+        # print(x.shape)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.bn2(x)
+        # print(x.shape)
+
+        x = x.view(batch_nodes, -1)
+        # print(x.shape)
+
+        h = self.recurrent(x, edge_index, H=hidden_state)
+        h = F.relu(h)
+
+        return h
+
 
 
 class GTS_Model(nn.Module):
@@ -12,28 +59,38 @@ class GTS_Model(nn.Module):
         super(GTS_Model, self).__init__()
 
         self.config = config
+        self.dataset_conf = config.dataset
 
         self.node_nums = config.nodes_num
+        self.embedding_dim = config.embedding.embedding_dim
+        self.decode_step = config.decode_step
+        self.decode_dim =config.decode_dim
 
         self.graph_learning = GTS_Graph_Learning(self.config)
         self.graph_forecasting = GTS_Spike_Decoding(self.config)
 
-        self.loss = config.train.loss_function
+        self.linear_1 = torch.nn.Linear(self.node_nums*self.embedding_dim, self.node_nums*self.embedding_dim//2)
 
-        if self.loss == 'CrossEntropy':
-            self.loss_func = nn.CrossEntropyLoss()
+        self.lin_reg = nn.ModuleList()
+        for _ in range(self.decode_dim):
+            self.lin_reg.append(torch.nn.Linear(self.node_nums*self.embedding_dim//2, 742))
 
-        elif self.loss == 'MSELoss':
-            self.loss_func = nn.MSELoss()
+        self.classification_1 = torch.nn.Linear(self.node_nums*self.embedding_dim//2,
+                                              self.node_nums*self.embedding_dim//8)
+        self.classification_2 = torch.nn.Linear(self.node_nums * self.embedding_dim // 8, 8)
 
-        elif self.loss == 'Multitask_Loss':
-            # self.CE_loss = nn.CrossEntropyLoss()
-            # self.mse_loss = nn.MSELoss()
-            pass
-        else:
-            raise ValueError("Non-supported loss function!")
 
-    def forward(self, inputs, targets, edge_index):
+    def sliding(self):
+        valid_sampling_locations = []
+        valid_sampling_locations += [
+            (self.dataset_conf.window_size + i)
+            for i in range(self.dataset_conf.total_time_length - self.dataset_conf.window_size + 1)
+            if (i % self.dataset_conf.slide) == 0
+        ]
+
+        return valid_sampling_locations
+
+    def forward(self, inputs, edge_index):
         adj = self.graph_learning(inputs, edge_index)
 
         edge_probability = F.gumbel_softmax(adj, tau=0.3, hard=True)
@@ -46,8 +103,25 @@ class GTS_Model(nn.Module):
 
         adj_matrix = torch.stack(edge_, dim=-1)
 
-        outputs = self.graph_forecasting(inputs, adj_matrix)
+        loc = self.sliding()
 
-        loss = self.loss_func(outputs, targets)
+        hidden_state=None
+        for start_idx in loc:
+            spike_inputs_per_window = inputs[:, start_idx - self.dataset_conf.window_size:start_idx]
+            hidden_state = self.graph_forecasting(spike_inputs_per_window, adj_matrix, hidden_state)
 
-        return adj_matrix, outputs, loss
+        hidden_state = hidden_state.view(-1)
+
+        out = self.linear_1(hidden_state)
+
+        angle_out = self.classification_1(out)
+        angle = self.classification_2(angle_out)
+
+        outputs = []
+        for ii in range(self.decode_dim):
+            output = self.lin_reg[ii](out)
+            outputs.append(output)
+
+        outputs = torch.stack(outputs, dim=0)
+
+        return adj_matrix, outputs, angle
--- runner/CCN_Project_runner.py
+++ runner/CCN_Project_runner.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from sklearn.model_selection import train_test_split
+
 from collections import defaultdict
 import torch.optim as optim
 from torch_geometric.data import Data
@@ -38,87 +40,87 @@ class GTS_Runner(object):
         if self.use_gpu and (self.device != 'cpu'):
             self.fully_connected_edge_index = self.fully_connected_edge_index.to(device=self.device)
 
-        if self.dataset_conf.name == 'monkey_data':
-            print("Load Spike Dataset")
-            spike = pickle.load(open('./data/CCN/monkey_spike.pickle', 'rb'))
-
-            print("Split Spike Train, Valid, Test Dataset")
-            self.train_entire = spike[:, :80, :, :]
-            self.valid_entire = spike[:, 80:90, :, :]
-            self.test_entire = spike[:, 90:, :, :]
-
-            self.train_dataset = self.prepare_dataset(self.train_entire)
-            self.valid_dataset = self.prepare_dataset(self.valid_entire)
-            self.test_dataset = self.prepare_dataset(self.test_entire)
-
-            self.train_entire = self.train_entire.reshape(-1, 98, 742)
-            self.valid_entire = self.valid_entire.reshape(-1, 98, 742)
-            self.test_entire = self.test_entire.reshape(-1, 98, 742)
-
-        else:
-            raise ValueError("Non-supported dataset!")
-
         self.model = GTS_Model(self.config)
+
+        self.CE_loss = nn.CrossEntropyLoss()
+        self.mse_loss = nn.MSELoss()
+        self.loss_ratio = config.loss_ratio
 
         if self.use_gpu and (self.device != 'cpu'):
             self.model = self.model.to(device=self.device)
 
-    def prepare_dataset(self, dataset):
-        valid_sampling_locations = []
-        valid_sampling_locations += [
-            (self.dataset_conf.window_size + i)
-            for i in range(self.dataset_conf.total_time_length - self.dataset_conf.window_size + 1)
-            if (i % self.dataset_conf.slide) == 0
-        ]
+        dataset = pickle.load(open('./data/CCN/monkeydata_with_class.pickle', 'rb'))
+        train_data, valid_data, test_data = self.split_train_valid_test(dataset)
 
+        self.train_dataset = self.make_dataset(train_data, edge_index=self.fully_connected_edge_index)
+        self.valid_dataset = self.make_dataset(valid_data, edge_index=self.fully_connected_edge_index)
+        self.test_dataset = self.make_dataset(test_data, edge_index=self.fully_connected_edge_index)
+
+    def split_train_valid_test(self, dataset):
+        train_x = []
+        train_y = []
+
+        valid_x = []
+        valid_y = []
+
+        test_x = []
+        test_y = []
+
+        for angle in range(8):
+            data = dataset[angle, :, :, :]
+            X_train, X_test, y_train, y_test = train_test_split(data[:, :98, :], data[:, 98:, :], test_size=0.2)
+            X_valid, X_test, y_valid, y_test = train_test_split(X_test, y_test, test_size=0.5)
+
+            train_x += X_train
+            train_y += y_train
+
+            valid_x += X_valid
+            valid_y += y_valid
+
+            test_x += X_test
+            test_y += y_test
+
+        train_x = torch.stack(train_x, dim=0)
+        train_y = torch.stack(train_y, dim=0)
+
+        valid_x = torch.stack(valid_x, dim=0)
+        valid_y = torch.stack(valid_y, dim=0)
+
+        test_x = torch.stack(test_x, dim=0)
+        test_y = torch.stack(test_y, dim=0)
+
+        return [train_x, train_y], [valid_x, valid_y], [test_x, test_y]
+
+    def make_dataset(self, dataset, edge_index):
+        data_len = dataset[0].shape[0]
         data_list = []
-        idx = 0
-        for target in range(8):
-            for trial in range(dataset.shape[1]):
-
-                spike_data = []
-                for start_idx in valid_sampling_locations:
-                    one_cell_inputs = dataset[target, trial, :,
-                                      (start_idx - self.dataset_conf.window_size):start_idx]
-                    spike_data.append(one_cell_inputs)
-
-                spike_inputs = np.stack(spike_data, axis=1)
-                data_item = Data(x=torch.FloatTensor(spike_inputs), edge_index=None, y=[target, idx])
-                data_list.append(data_item)
-                idx += 1
+        for trial in range(data_len):
+            data_item = Data(x=torch.FloatTensor(dataset[0][trial, :, :]), edge_index=edge_index,
+                             y=torch.FloatTensor(dataset[1][trial, :, :]))
+            data_list.append(data_item)
 
         return data_list
+
+    def compute_loss(self, reg_prediction, class_prediction, target, loss_ratio):
+        cost_x = self.mse_loss(reg_prediction[0,:], target[0,:])
+        cost_y = self.mse_loss(reg_prediction[1, :], target[1, :])
+        cost_z = self.mse_loss(reg_prediction[2, :], target[2, :])
+
+        reg_cost = (cost_x+cost_y+cost_z) / 3 / target.shape[1]
+
+        classification_cost = self.CE_loss(class_prediction.reshape(1,8), target[-1,:1].to(torch.long))
+
+        loss = loss_ratio*reg_cost + (1-loss_ratio)*classification_cost
+        return loss
+
 
     def train(self):
         print('Train Start')
         train_loader = DataLoader(self.train_dataset, batch_size=self.train_conf.batch_size, shuffle=True)
         valid_loader = DataLoader(self.valid_dataset, batch_size=self.train_conf.batch_size, shuffle=True)
 
-        # create optimizer
-        params = filter(lambda p: p.requires_grad, self.model.parameters())
-        if self.train_conf.optimizer == 'SGD':
-            optimizer = optim.SGD(
-                params,
-                lr=self.train_conf.lr,
-                momentum=self.train_conf.momentum,
-                weight_decay=self.train_conf.wd)
-        elif self.train_conf.optimizer == 'Adam':
-            optimizer = optim.Adam(
-                params,
-                lr=self.train_conf.lr,
-                weight_decay=self.train_conf.wd)
-        else:
-            raise ValueError("Non-supported optimizer!")
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.01)
 
-        lr_scheduler = optim.lr_scheduler.MultiStepLR(
-            optimizer,
-            milestones=self.train_conf.lr_decay_steps,
-            gamma=self.train_conf.lr_decay)
-
-        # reset gradient
-        optimizer.zero_grad()
-
-        iter_count = 0
         results = defaultdict(list)
         best_val_loss = np.inf
 
@@ -128,46 +130,20 @@ class GTS_Runner(object):
             self.model.train()
             train_loss = []
 
-            iter = 0
             for data_batch in tqdm(train_loader):
-                target = np.stack(data_batch.y, axis=0)[:, 0]
-                target = torch.Tensor(target).to(torch.int64)
-                # target = F.one_hot(torch.Tensor(target).to(torch.int64), num_classes=8)
-                # target = torch.FloatTensor(target)
-
-                indexs = np.stack(data_batch.y, axis=0)[:, 1]
-
-                gl = [self.train_entire[i] for i in indexs]
-                entire_input = torch.FloatTensor(np.array(gl))
-
                 if self.use_gpu and (self.device != 'cpu'):
                     data_batch = data_batch.to(device=self.device)
-                    target = target.to(device=self.device)
-                    entire_input = entire_input.to(device=self.device)
 
-                # print(f'data_batch.x: {data_batch.x.shape}')
-                # print(f'data_batch.x: {target.shape}')
-                # print(f'entire_inputs: {entire_input.shape}')
-                # print(f'fully_connected_edge_index.x: {self.fully_connected_edge_index.shape}')
+                _, outputs, angle = self.model(inputs=data_batch.x, edge_index=data_batch.edge_index)
 
-                _, _, loss = self.model(inputs=data_batch.x, targets=target,
-                                        entire_inputs=entire_input,
-                                        edge_index=self.fully_connected_edge_index)
-
+                loss = self.compute_loss(outputs, angle, data_batch.y, self.loss_ratio)
                 # backward pass (accumulates gradients).
                 loss.backward()
                 # performs a single update step.
                 optimizer.step()
+                optimizer.zero_grad()
 
                 train_loss += [float(loss.data.cpu().numpy())]
-                iter += 1
-
-                # display loss
-                if (iter + 1) % 100 == 0:
-                    logger.info(
-                        "Train Loss @ epoch {} iteration {} = {}".format(epoch + 1, iter + 1, float(loss.data.cpu().numpy())))
-
-            lr_scheduler.step()
 
             train_loss = np.stack(train_loss).mean()
             results['train_loss'] += [train_loss]
@@ -179,34 +155,20 @@ class GTS_Runner(object):
             total = 0
             correct = 0
             for data_batch in tqdm(valid_loader):
-                target = np.stack(data_batch.y, axis=0)[:, 0]
-                target = torch.Tensor(target).to(torch.int64)
-                # target = F.one_hot(torch.Tensor(target).to(torch.int64), num_classes=8)
-
-                indexs = np.stack(data_batch.y, axis=0)[:, 1]
-                gl = [self.train_entire[i] for i in indexs]
-                entire_input = torch.FloatTensor(np.array(gl))
-
                 if self.use_gpu and (self.device != 'cpu'):
                     data_batch = data_batch.to(device=self.device)
-                    target = target.to(device=self.device)
-                    entire_input = entire_input.to(device=self.device)
 
                 with torch.no_grad():
-                    adj_matrix, out, loss = self.model(inputs=data_batch.x, targets=target,
-                                                       entire_inputs=entire_input,
-                                                       edge_index=self.fully_connected_edge_index)
+                    _, outputs, angle = self.model(inputs=data_batch.x, edge_index=data_batch.edge_index)
+
+                loss = self.compute_loss(outputs, angle, data_batch.y, self.loss_ratio)
+
                 val_loss += [float(loss.data.cpu().numpy())]
 
-                _, pred = torch.max(out.data, 1)
-                total += target.size(0)
-                correct += (pred == target).sum().item()
-
             val_loss = np.stack(val_loss).mean()
-
             results['val_loss'] += [val_loss]
-            results['val_adj_matirix'] += [adj_matrix]
-            results['val_acc'] += [correct / total]
+
+            results['valid_classification_acc'] += [correct / total]
 
             logger.info("Avg. Validation Loss = {:.6}".format(val_loss, 0))
             logger.info("Current Best Validation Loss = {:.6}".format(best_val_loss))
@@ -240,35 +202,26 @@ class GTS_Runner(object):
         correct = 0
 
         for data_batch in tqdm(test_loader):
-            target = np.stack(data_batch.y, axis=0)[:, 0]
-            target = torch.Tensor(target).to(torch.int64)
-            # target = F.one_hot(torch.Tensor(target).to(torch.int64), num_classes=8)
-
-            indexs = np.stack(data_batch.y, axis=0)[:, 1]
-            gl = [self.train_entire[i] for i in indexs]
-            entire_input = torch.FloatTensor(np.array(gl))
-
             if self.use_gpu and (self.device != 'cpu'):
                 data_batch = data_batch.to(device=self.device)
-                target = target.to(device=self.device)
-                entire_input = entire_input.to(device=self.device)
 
             with torch.no_grad():
-                adj_matrix, out, loss = self.best_model(inputs=data_batch.x, targets=target,
-                                                   entire_inputs=entire_input,
-                                                   edge_index=self.fully_connected_edge_index)
+                adj_matrix, out, angle = self.model(inputs=data_batch.x, edge_index=data_batch.edge_index)
+
+            loss = self.compute_loss(out, angle, data_batch.y, self.loss_ratio)
 
             test_loss += [float(loss.data.cpu().numpy())]
-            _, pred = torch.max(out.data, 1)
-            total += target.size(0)
-            correct += (pred == target).sum().item()
+
+            _, pred = torch.max(angle.data, 1)
+            correct += (pred == data_batch.y[-1, 0]).sum().item()
+            total += 1
 
 
         test_loss = np.stack(test_loss).mean()
 
         results['test_loss'] += [test_loss]
         results['adj_matrix'] = adj_matrix.cpu().numpy()
-        results['test_acc'] += [correct / total]
+        results['test_classification_acc'] += [correct / total]
 
 
         logger.info("Avg. Test Loss = {:.6}".format(test_loss, 0))
