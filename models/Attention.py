0966d0780b5911486a94d06e6543096df6909773
Kim-Ju-Jyeon <wngus1310@naver.com>
2022-02-21 00:17:32 +0900

    attention graph learning

---
+++ models/Attention.py
@@ -0,0 +1,71 @@
+from torch import nn
+import torch
+from torch.nn import functional as F
+
+
+class GraphLearningScaledDotProductAttention(nn.Module):
+    def __init__(self, dropout=0):
+        super(GraphLearningScaledDotProductAttention, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, q, k, mask=None):
+        attn = torch.bmm(q, k.permute(0, 2, 1))
+
+        dimention = torch.sqrt(torch.tensor(k.shape[-1]).to(torch.float32))
+        attn = attn / dimention
+
+        if mask is not None:
+            attn = attn.masked_fill(mask == 0, -1e9)
+
+        attn = self.softmax(attn)
+        attn = self.dropout(attn)
+
+        return attn
+
+
+class GraphLearningMultiHeadAttention(nn.Module):
+    def __init__(self, n_head, d_model, dropout_rate=0.5):
+        super(GraphLearningMultiHeadAttention, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+
+        self.d_k = self.d_q = d_model // n_head
+        self.dropout = nn.Dropout(p=dropout_rate)
+
+        self.q_layers = nn.ModuleList([nn.Linear(self.d_model, self.d_q, bias=False)
+                                       for _ in range(self.n_head)])
+        self.k_layers = nn.ModuleList([nn.Linear(self.d_model, self.d_k, bias=False)
+                                       for _ in range(self.n_head)])
+
+        self.attention = GraphLearningScaledDotProductAttention()
+
+        self.output = nn.Linear(d_model, d_model * n_head)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for name, p in self.named_parameters():
+            if 'bias' not in name:
+                torch.nn.init.xavier_uniform_(p)
+            else:
+                torch.nn.init.zeros_(p)
+
+    def forward(self, q, k, mask=None):
+        batch_size, nodes_num, _ = q.shape
+        _attns = []
+        for i in range(self.n_head):
+            qs = F.relu(self.q_layers[i](q))
+            ks = F.relu(self.k_layers[i](k))
+
+            attn = self.attention(qs, ks, mask)
+
+            attn_dropout = self.dropout(attn)
+            _attns.append(attn_dropout)
+
+        attn = torch.stack(_attns)
+        attn = attn.reshape(batch_size, nodes_num, -1)
+        outputs = self.output(attn)
+
+        return outputs, _attns
---
+++ models/GTS/self_attention_graph_learning.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from models.Attention import GraphLearningMultiHeadAttention
+
+
+class Attention_Graph_Learning(nn.Module):
+    def __init__(self, config):
+        super(Attention_Graph_Learning, self).__init__()
+        self.num_nodes = config.nodes_num
+        self.nodes_feas = config.node_features
+
+        self.mode = config.graph_learning.mode
+
+        self.total_length = config.dataset.graph_learning_length
+        self.kernel_size = config.graph_learning.kernel_size
+        self.stride = config.graph_learning.stride
+        self.conv1_dim = config.graph_learning.conv1_dim
+        self.conv2_dim = config.graph_learning.conv2_dim
+        self.conv3_dim = config.graph_learning.conv3_dim
+        self.hidden_dim = config.hidden_dim
+
+        self.attention_gl = GraphLearningMultiHeadAttention(config.graph_learning.n_head, self.hidden_dim)
+
+        out_size = 0
+        for i in range(len(self.kernel_size)):
+            if i == 0:
+                out_size = int(((self.total_length - self.kernel_size[i]) / self.stride[i]) + 1)
+            else:
+                out_size = int(((out_size - self.kernel_size[i]) / self.stride[i]) + 1)
+
+        self.conv1 = torch.nn.Conv1d(self.nodes_feas, self.conv1_dim, self.kernel_size[0], stride=self.stride[0])
+        self.conv2 = torch.nn.Conv1d(self.conv1_dim, self.conv2_dim, self.kernel_size[1], stride=self.stride[1])
+        self.conv3 = torch.nn.Conv1d(self.conv2_dim, self.conv3_dim, self.kernel_size[2], stride=self.stride[2])
+
+        self.fc_conv = torch.nn.Conv1d(self.conv3_dim, 1, 1, stride=1)
+
+        self.fc = nn.Linear(out_size, self.hidden_dim)
+
+        self.hidden_drop = nn.Dropout(0.4)
+
+        self.bn1 = torch.nn.BatchNorm1d(self.conv1_dim)
+        self.bn2 = torch.nn.BatchNorm1d(self.conv2_dim)
+        self.bn3 = torch.nn.BatchNorm1d(self.conv3_dim)
+
+    def init_weights(self):
+        for m in self.modules():
+            if (isinstance(m, nn.Linear)) or (isinstance(m, nn.Conv1d)):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.fill_(0.1)
+
+    def forward(self, x):
+        if len(x.shape) == 2:
+            x = x.reshape(self.num_nodes, 1, -1)
+
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.bn1(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.bn2(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.bn3(x)
+
+        x = self.fc_conv(x)
+        x = F.relu(x)
+
+        x = x.squeeze()
+        x = self.fc(x)
+        x = F.relu(x)
+
+        outputs, attn = self.attention_gl(x)
+
+        return outputs, attn
