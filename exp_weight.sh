8841fcd736fd97539b467210f943902629fbe87a
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-03-11 12:07:00 +0900

    exp

--- exp_weight.sh
+++ exp_weight.sh
@@ -7,9 +7,9 @@ do
     sleep 3
 done
 
-export CUDA_VISIBLE_DEVICES=1
-for i in 2
-do
-    python3 run_exp_dgx.py --conf_file_path ./config/GTS/0310_dgx_attention/$i.yaml &
-    sleep 3
-done
+#export CUDA_VISIBLE_DEVICES=1
+#for i in 2
+#do
+#    python3 run_exp_dgx.py --conf_file_path ./config/GTS/0310_dgx_attention/$i.yaml &
+#    sleep 3
+#done
