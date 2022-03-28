5130e1d005483dd35dec0f6520f88b96331a05b5
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-03-28 11:01:17 +0900

    Exp setting

--- GSL/exp.sh
+++ GSL/exp.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=0
-for i in 1
+#export CUDA_VISIBLE_DEVICES=0
+#for i in 1
+#do
+#    python3 run_exp_dgx.py --conf_file_path ./config/GTS/METR-LA/$i.yaml &
+#    sleep 3
+#done
+
+export CUDA_VISIBLE_DEVICES=1
+for i in 2
 do
     python3 run_exp_dgx.py --conf_file_path ./config/GTS/METR-LA/$i.yaml &
     sleep 3
 done
 
-#export CUDA_VISIBLE_DEVICES=1
-#for i in 2
-#do
-#    python3 run_exp_dgx.py --conf_file_path ./config/GTS/METR-LA/$i.yaml &
-#    sleep 3
-#done
-#
-#export CUDA_VISIBLE_DEVICES=2
-#for i in 3
-#do
-#    python3 run_exp_dgx.py --conf_file_path ./config/GTS/METR-LA/$i.yaml &
-#    sleep 3
-#done
+export CUDA_VISIBLE_DEVICES=2
+for i in 3
+do
+    python3 run_exp_dgx.py --conf_file_path ./config/GTS/METR-LA/$i.yaml &
+    sleep 3
+done
+
+export CUDA_VISIBLE_DEVICES=3
+for i in 1
+do
+    python3 run_exp_dgx.py --conf_file_path ./config/GTS/PEMS-BAY/$i.yaml &
+    sleep 3
+done
