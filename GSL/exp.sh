fc9a30908e89a5728ddf05dd8acdca752bf83805
JuHyeon Kim <juhyeonkim@JuHyeonui-MacBookPro.local>
2022-04-07 19:17:11 +0900

    yaml file

--- GSL/exp.sh
+++ GSL/exp.sh
@@ -3,35 +3,35 @@
 export CUDA_VISIBLE_DEVICES=0
 for i in 1
 do
-    python3 run_exp.py --conf_file_path ./config/GTS/config/attention_Gumbel.yaml &
+    python3 run_exp.py --conf_file_path ./config/0407_Exp/attention_Gumbel.yaml &
     sleep 3
-    python3 run_exp.py --conf_file_path ./config/GTS/config/attention_Top_k.yaml &
+    python3 run_exp.py --conf_file_path ./config/0407_Exp/attention_Top_k.yaml &
     sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=3
 for i in 1
 do
-    python3 run_exp.py --conf_file_path ./config/GTS/config/attention_weight.yaml &
+    python3 run_exp.py --conf_file_path ./config/0407_Exp/attention_weight.yaml &
     sleep 3
-    python3 run_exp.py --conf_file_path ./config/GTS/config/GTS_Gumbel.yaml &
+    python3 run_exp.py --conf_file_path ./config/0407_Exp/GTS_Gumbel.yaml &
     sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=2
 for i in 1
 do
-    python3 run_exp.py --conf_file_path ./config/GTS/config/GTS_Top_k.yaml &
+    python3 run_exp.py --conf_file_path ./config/0407_Exp/GTS_Top_k.yaml &
     sleep 3
-    python3 run_exp.py --conf_file_path ./config/GTS/config/GTS_weight.yaml &
+    python3 run_exp.py --conf_file_path ./config/0407_Exp/GTS_weight.yaml &
     sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=1
 for i in 1
 do
-    python3 run_exp.py --conf_file_path ./config/GTS/config/MTGNN_Gumbel.yaml &
+    python3 run_exp.py --conf_file_path ./config/0407_Exp/MTGNN_Gumbel.yaml &
     sleep 3
-    python3 run_exp.py --conf_file_path ./config/GTS/config/MTGNN_Top_k.yaml &
+    python3 run_exp.py --conf_file_path ./config/0407_Exp/MTGNN_Top_k.yaml &
     sleep 3
 done
