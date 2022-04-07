1f73e6b05929535ed17a130510d0e8a2627472aa
JuHyeon Kim <juhyeonkim@JuHyeonui-MacBookPro.local>
2022-04-07 19:29:43 +0900

    Exp list update

--- GSL/exp.sh
+++ GSL/exp.sh
@@ -3,8 +3,8 @@
 export CUDA_VISIBLE_DEVICES=0
 for i in 1
 do
-    python3 run_exp.py --conf_file_path ./config/0407_Exp/attention_Gumbel.yaml &
-    sleep 3
+#    python3 run_exp.py --conf_file_path ./config/0407_Exp/attention_Gumbel.yaml &
+#    sleep 3
     python3 run_exp.py --conf_file_path ./config/0407_Exp/attention_Top_k.yaml &
     sleep 3
 done
@@ -12,8 +12,8 @@ done
 export CUDA_VISIBLE_DEVICES=3
 for i in 1
 do
-    python3 run_exp.py --conf_file_path ./config/0407_Exp/attention_weight.yaml &
-    sleep 3
+#    python3 run_exp.py --conf_file_path ./config/0407_Exp/attention_weight.yaml &
+#    sleep 3
     python3 run_exp.py --conf_file_path ./config/0407_Exp/GTS_Gumbel.yaml &
     sleep 3
 done
@@ -23,15 +23,15 @@ for i in 1
 do
     python3 run_exp.py --conf_file_path ./config/0407_Exp/GTS_Top_k.yaml &
     sleep 3
-    python3 run_exp.py --conf_file_path ./config/0407_Exp/GTS_weight.yaml &
-    sleep 3
+#    python3 run_exp.py --conf_file_path ./config/0407_Exp/GTS_weight.yaml &
+#    sleep 3
 done
 
 export CUDA_VISIBLE_DEVICES=1
 for i in 1
 do
-    python3 run_exp.py --conf_file_path ./config/0407_Exp/MTGNN_Gumbel.yaml &
-    sleep 3
+#    python3 run_exp.py --conf_file_path ./config/0407_Exp/MTGNN_Gumbel.yaml &
+#    sleep 3
     python3 run_exp.py --conf_file_path ./config/0407_Exp/MTGNN_Top_k.yaml &
     sleep 3
 done
