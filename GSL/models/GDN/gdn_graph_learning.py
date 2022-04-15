c264a40076f16fbe25fe5a68ec43d70ad248eda2
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-04-15 17:59:37 +0900

    GDN graph learning module !ing

--- GSL/models/GDN/gdn_graph_learning.py
+++ GSL/models/GDN/gdn_graph_learning.py
@@ -14,22 +14,17 @@ class MTGNN_Graph_Learning(nn.Module):
         super(MTGNN_Graph_Learning, self).__init__()
 
         self.num_nodes = config.nodes_num
-        dim = config.graph_learning.hidden_dim
+        self.dim = config.graph_learning.hidden_dim
 
         self.sampling = config.graph_learning.sampling
 
-        self._embedding1 = nn.Embedding(self.num_nodes, dim)
-        self._embedding2 = nn.Embedding(self.num_nodes, dim)
-        self._linear1 = nn.Linear(dim, dim)
-        self._linear2 = nn.Linear(dim, dim)
+        self._embedding1 = nn.Embedding(self.num_nodes, self.dim)
 
         if self.sampling == 'Gumbel_softmax':
             self.gumbel_trick = nn.Linear(1, 2)
 
-        self._alpha = config.graph_learning.alpha
 
         self._reset_parameters()
-
 
     def _reset_parameters(self):
         for p in self.parameters():
@@ -39,19 +34,11 @@ class MTGNN_Graph_Learning(nn.Module):
                 nn.init.uniform_(p)
 
     def forward(self) -> torch.Tensor:
-        nodevec1 = self._embedding1(torch.arange(self.num_nodes))
-        nodevec2 = self._embedding2(torch.arange(self.num_nodes))
+        nodevec = self._embedding1(torch.arange(self.num_nodes))
 
-        nodevec1 = torch.tanh(self._alpha * self._linear1(nodevec1))
-        nodevec2 = torch.tanh(self._alpha * self._linear2(nodevec2))
-
-        outputs = torch.mm(nodevec1, nodevec2.transpose(1, 0)) - torch.mm(
-            nodevec2, nodevec1.transpose(1, 0)
-        )
 
         if self.sampling == 'Gumbel_softmax':
             outputs = self.gumbel_trick(outputs.unsqueeze(dim=-1))
 
-        outputs = F.relu(torch.tanh(self._alpha * outputs))
 
         return outputs
