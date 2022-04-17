2c08afed0cf1164a0d2b25442929996a5d53fddd
JuHyeon Kim <juhyeonkim@JuHyeonui-MacBookPro.local>
2022-04-17 18:19:31 +0900

    Top-K sampling exp

--- GSL/exp_rtx90.sh
+++ GSL/exp_rtx90.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-#export CUDA_VISIBLE_DEVICES=0
-#for i in 1
-#do
-#    python3 run_exp.py --conf_file_path ./config/0416/attention_Gumbel.yaml &
-#    sleep 3
-#done
+export CUDA_VISIBLE_DEVICES=0
+for i in 1
+do
+    python3 run_exp.py --conf_file_path ./config/Top_K/attention_Top_k.yaml &
+    sleep 3
+done
 
-#export CUDA_VISIBLE_DEVICES=3
-#for i in 1
-#do
-#    python3 run_exp.py --conf_file_path ./config/0416/attention_weight.yaml &
-#    sleep 3
-#done
+export CUDA_VISIBLE_DEVICES=3
+for i in 1
+do
+    python3 run_exp.py --conf_file_path ./config/Top_K/GTS_Top_k.yaml &
+    sleep 3
+done
 
 export CUDA_VISIBLE_DEVICES=2
 for i in 1
 do
-    python3 run_exp.py --conf_file_path ./config/0416/GTS_weight.yaml &
+    python3 run_exp.py --conf_file_path ./config/Top_K/MTGNN_Top_k.yaml &
     sleep 3
 done
 
