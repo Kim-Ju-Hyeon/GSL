f81aa8dded2f201eeff5158cea1572566ff2a355
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-04-18 16:25:45 +0900

    batch_size

--- GSL/exp_rtx90.sh
+++ GSL/exp_rtx90.sh
@@ -14,12 +14,12 @@ done
 #    sleep 3
 #done
 
-export CUDA_VISIBLE_DEVICES=2
-for i in 1
-do
-    python3 run_exp.py --conf_file_path ./config/Top_K_masking_zero/MTGNN_Top_k.yaml &
-    sleep 3
-done
+#export CUDA_VISIBLE_DEVICES=2
+#for i in 1
+#do
+#    python3 run_exp.py --conf_file_path ./config/Top_K_masking_zero/MTGNN_Top_k.yaml &
+#    sleep 3
+#done
 
 #export CUDA_VISIBLE_DEVICES=1
 #for i in 1
