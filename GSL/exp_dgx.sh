c7463cbe680b00ad4aeff71b602585a5812be31d
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-05-17 18:30:45 +0900

    n-beats exp setting

--- GSL/exp_dgx.sh
+++ GSL/exp_dgx.sh
@@ -7,23 +7,23 @@
 #    sleep 3
 #done
 
-#export CUDA_VISIBLE_DEVICES=3
-#for i in 1
-#do
-#    python3 run_exp.py --conf_file_path ./config/No_Graph_Learning/groud_truth.yaml &
-#    sleep 3
-#done
-#
-#export CUDA_VISIBLE_DEVICES=2
-#for i in 1
-#do
-#    python3 run_exp.py --conf_file_path ./config/No_Graph_Learning/no_graph.yaml &
-#    sleep 3
-#done
-#
-#export CUDA_VISIBLE_DEVICES=1
-#for i in 1
-#do
-#    python3 run_exp.py --conf_file_path ./config/No_Graph_Learning/random_graph.yaml &
-#    sleep 3
-#done
+export CUDA_VISIBLE_DEVICES=3
+for i in 1
+do
+    python3 run_exp.py --conf_file_path ./config/N_BERATS/nbeats_generic.yaml &
+    sleep 3
+done
+
+export CUDA_VISIBLE_DEVICES=2
+for i in 1
+do
+    python3 run_exp.py --conf_file_path ./config/N_BERATS/nbeats_trend_season.yaml &
+    sleep 3
+done
+
+export CUDA_VISIBLE_DEVICES=1
+for i in 1
+do
+    python3 run_exp.py --conf_file_path ./config/N_BERATS/nbeats_trend_season2.yaml &
+    sleep 3
+done
