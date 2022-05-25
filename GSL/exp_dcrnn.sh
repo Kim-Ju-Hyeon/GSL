ecfdaeb000acf77148a38d3bcff5eb94fd6ead55
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-05-25 15:38:36 +0900

    DCRNN 12step exp

--- GSL/exp_dcrnn.sh
+++ GSL/exp_dcrnn.sh
@@ -3,8 +3,45 @@
 export CUDA_VISIBLE_DEVICES=2
 python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 12 --hidden_dim 64 &
 sleep 3
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 24 --hidden_dim 64 &
+sleep 3
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 36 --hidden_dim 64 &
+sleep 3
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 48 --hidden_dim 64 &
+sleep 3
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 60 --hidden_dim 64 &
+sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 12 --hidden_dim 128 &
+#sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 24 --hidden_dim 128 &
+#sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 36 --hidden_dim 128 &
+#sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 48 --hidden_dim 128 &
+#sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 60 --hidden_dim 128 &
+#sleep 3
+
 
 
 export CUDA_VISIBLE_DEVICES=3
-python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 12 --hidden_dim 64 &
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 12 --hidden_dim 32 &
 sleep 3
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 24 --hidden_dim 32 &
+sleep 3
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 36 --hidden_dim 32 &
+sleep 3
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 48 --hidden_dim 32 &
+sleep 3
+python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 60 --hidden_dim 32 &
+sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 12 --hidden_dim 256 &
+#sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 24 --hidden_dim 256 &
+#sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 36 --hidden_dim 256 &
+#sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 48 --hidden_dim 256 &
+#sleep 3
+#python3 run_exp.py --conf_file_path ./config/DCRNN/dcrnn.yaml --input_length 60 --hidden_dim 256 &
+#sleep 3
