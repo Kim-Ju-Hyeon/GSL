cf7cba9ebe6a08b1a1f1d438a920402e3d3c5903
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-06-03 17:09:55 +0900

    MPNN exp

--- GSL/exp_dgx.sh
+++ GSL/exp_dgx.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 export CUDA_VISIBLE_DEVICES=0
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic --num_blocks_per_stack 1 --n_theta_hidden 128,128 --thetas_dim 64,32 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic --num_blocks_per_stack 1 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
 sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128 --thetas_dim 64,32 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
 sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 3 --n_theta_hidden 128,128 --thetas_dim 64,32 &
-sleep 3
-
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 1 --n_theta_hidden 128,128 --thetas_dim 64,32 &
-sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128 --thetas_dim 64,32 &
-sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic ,generic,generic,generic --num_blocks_per_stack 3 --n_theta_hidden 128,128 --thetas_dim 64,32 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 3 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
 sleep 3
 
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 1 --n_theta_hidden 128,128 --thetas_dim 64,32 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 1 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
 sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128 --thetas_dim 64,32 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
 sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 3 --n_theta_hidden 128,128 --thetas_dim 64,32 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic ,generic,generic,generic --num_blocks_per_stack 3 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
+sleep 3
+
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 1 --n_theta_hidden 128,128,128--thetas_dim 64,32 &
+sleep 3
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
+sleep 3
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 3 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
 sleep 3
