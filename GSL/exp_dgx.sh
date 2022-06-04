90f386620ab4ad72750308973ee7879fecaf7e8f
JuHyeon Kim <juhyeonkim@JuHyeonui-MacBookPro.local>
2022-06-05 02:01:34 +0900

    grid search exp

--- GSL/exp_dgx.sh
+++ GSL/exp_dgx.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 
 export CUDA_VISIBLE_DEVICES=0
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic --num_blocks_per_stack 1 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --type MPNN
 sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --type MPGLU
 sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 3 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
-sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 1 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
-sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
-sleep 3
+
 
 export CUDA_VISIBLE_DEVICES=1
-
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic ,generic,generic,generic --num_blocks_per_stack 3 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --type MP_single_message
 sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 1 --n_theta_hidden 128,128,128--thetas_dim 64,32 &
-sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 2 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
-sleep 3
-python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 3 --n_theta_hidden 128,128,128 --thetas_dim 64,32 &
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --type MPGLU_single_message
 sleep 3
---
+++ GSL/grid_search_nbeats_exp.py
@@ -0,0 +1,67 @@
+import click
+from runner.runner import Runner
+
+from utils.train_helper import get_config, save_yaml
+import traceback
+from utils.logger import setup_logging
+import os
+
+from utils.slack import slack_message
+from utils.train_helper import set_seed, mkdir, edict2dict
+import datetime
+
+import pytz
+from easydict import EasyDict as edict
+import yaml
+
+
+@click.command()
+@click.option('--conf_file_path', type=click.STRING, default=None)
+@click.option('--type', type=click.STRING, default=None)
+def main(conf_file_path, type):
+    num_stack_list = [1, 3, 9, 15, 30, 45, 90]
+    num_blocks_per_stack_list = [1, 3, 9, 15, 30, 45, 90]
+    mlp_stack_list = [[128, 64, 32], [256, 128, 64],[16,16,16], [16,16,16,16,16] ,[32,32,32], [64,64,64]]
+    thetas_dim_list = [[64,64], [16,16], [8,8], [128,128], [60, 12]]
+
+    for n_stack in num_stack_list:
+        for n_block in num_blocks_per_stack_list:
+            for mlp_stack in mlp_stack_list:
+                for thetas_dim in thetas_dim_list:
+                    hyperparameter = f'stacks_{n_stack}__num_blocks_per_stack_{n_block}__n_theta_hidden_{mlp_stack}__thetas_dim_{thetas_dim}'
+
+                    start = datetime.datetime.now()
+                    start = start + datetime.timedelta(hours=9)
+
+                    config = edict(yaml.load(open(conf_file_path, 'r'), Loader=yaml.FullLoader))
+                    config.exp_name = type
+                    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+                    config.seed = set_seed(config.seed)
+
+                    config.exp_dir = os.path.join(config.exp_dir, str(config.exp_name))
+                    config.exp_sub_dir = os.path.join(config.exp_dir, config.model_name, hyperparameter)
+                    config.model_save = os.path.join(config.exp_sub_dir, "model_save")
+
+                    mkdir(config.model_save)
+
+                    config.forecasting_module.inter_correlation_block_type = type
+                    config.forecasting_module.num_blocks_per_stack = n_block
+                    config.forecasting_module.n_theta_hidden = mlp_stack
+                    config.forecasting_module.thetas_dim = thetas_dim
+                    config.forecasting_module.stack_type = ['gemeric'] * n_stack
+
+                    save_name = os.path.join(config.exp_sub_dir, 'config.yaml')
+                    yaml.dump(edict2dict(config), open(save_name, 'w'), default_flow_style=False)
+
+                    log_file = os.path.join(config.exp_sub_dir, "log_exp_{}.txt".format(config.seed))
+                    logger = setup_logging('INFO', log_file)
+                    logger.info("Writing log file to {}".format(log_file))
+                    logger.info("Exp instance id = {}".format(config.exp_name))
+
+                    runner = Runner(config=config)
+                    runner.train()
+                    runner.test()
+
+
+if __name__ == '__main__':
+    main()
