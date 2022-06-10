7fb51bf70436e99a926c572de9d5ff60f44aca2e
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-06-10 15:17:26 +0900

    exp rtx90

--- GSL/exp_rtx90.sh
+++ GSL/exp_rtx90.sh
@@ -2,17 +2,17 @@
 
 
 export CUDA_VISIBLE_DEVICES=3
-python3 run_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64,64,64 --thetas_dim 8,16 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64,64,64 --thetas_dim 8,16 &
 sleep 3
-python3 run_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64,64 --thetas_dim 8,16 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64,64 --thetas_dim 8,16 &
 sleep 3
-python3 run_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64 --thetas_dim 8,16 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64 --thetas_dim 8,16 &
 sleep 3
-python3 run_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64 --thetas_dim 8,16 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64 --thetas_dim 8,16 &
 sleep 3
-python3 run_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64 --thetas_dim 8,16 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64 --thetas_dim 8,16 &
 sleep 3
-python3 run_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64 --thetas_dim 8,16 &
+python3 run_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --stack_type generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic,generic  --num_blocks_per_stack 9 --n_theta_hidden 64,64 --thetas_dim 8,16 &
 sleep 3
 
 
