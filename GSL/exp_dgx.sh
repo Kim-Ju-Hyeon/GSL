26a44e0483394a9e5c3e4571b2d6e5309e60dfdb
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-06-20 16:35:36 +0900

    nihits exp

--- GSL/exp_dgx.sh
+++ GSL/exp_dgx.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 export CUDA_VISIBLE_DEVICES=0
-python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --type MPNN &
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_n_hits.yaml --type MPGLU --n_stack 3 --n_block 3 &
+sleep 30
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_n_hits.yaml --type MPGLU --n_stack 3 --n_block 9 &
 sleep 3
-python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --type MPGLU &
-sleep 3
+
 
 
 export CUDA_VISIBLE_DEVICES=1
-python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --type MP_single_message &
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_n_hits.yaml --type MPGLU --n_stack 9 --n_block 3 &
 sleep 3
-python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_generic.yaml --type MPGLU_single_message &
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_n_hits.yaml --type MPGLU --n_stack 9 --n_block 9 &
 sleep 3
