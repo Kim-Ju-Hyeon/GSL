bf8c86facedcfa49ca5d819dd09b8e102b882b56
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-06-21 20:20:32 +0900

    N-Hits Exp Setting

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -2,7 +2,7 @@
 
 
 export CUDA_VISIBLE_DEVICES=0
-python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_n_hits.yaml --n_stack 3 --n_block 3 --kernel_size 5 --inter_correlation_stack_length 1 &
+python3 grid_search_nbeats_exp.py --conf_file_path ./config/N_BEATS/nbeats_n_hits.yaml --n_stack 3 --n_block 3 --kernel_size 4 --inter_correlation_stack_length 1 &
 #sleep 3
 #
 #export CUDA_VISIBLE_DEVICES=1
