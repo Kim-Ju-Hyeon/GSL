adb4b1245904040cadec026d3aea7aab5c7d9fb5
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-07-12 15:48:42 +0900

    Stack size 32 is too big to exp @Octo with Batch 64 make Batch 32

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python3 run_dataset.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml
-sleep 240
+#python3 run_dataset.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml
+#sleep 240
 
 export CUDA_VISIBLE_DEVICES=0
 python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 32 --n_block 1 --mlp_stack 512 &
@@ -15,9 +15,9 @@ sleep 3
 #python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 9 --n_block 1 --mlp_stack 512 &
 #sleep 3
 
-export CUDA_VISIBLE_DEVICES=3
-python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 9 --n_block 1 --mlp_stack 512,512 &
-sleep 3
+#export CUDA_VISIBLE_DEVICES=3
+#python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 9 --n_block 1 --mlp_stack 512,512 &
+#sleep 3
 
 #export CUDA_VISIBLE_DEVICES=4
 #python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 3 --n_block 1 --mlp_stack 512 &
