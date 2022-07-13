df33142861ae10c16fde48abeeb933c50f526935
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-07-13 16:54:10 +0900

    PN_BEATS exp setting @ Octo

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -4,33 +4,33 @@
 #sleep 240
 
 export CUDA_VISIBLE_DEVICES=0
-python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 32 --n_block 1 --mlp_stack 512 &
+python3 grid_search_pnbeats__exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 1 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 32 --n_block 1 --mlp_stack 512,512 &
+python3 grid_search_pnbeats__exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 6 --singular_stack_num 1 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 &
 sleep 3
 
-#export CUDA_VISIBLE_DEVICES=2
-#python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 9 --n_block 1 --mlp_stack 512 &
-#sleep 3
+export CUDA_VISIBLE_DEVICES=2
+python3 grid_search_pnbeats__exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 9 --singular_stack_num 1 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 &
+sleep 3
 
-#export CUDA_VISIBLE_DEVICES=3
-#python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 9 --n_block 1 --mlp_stack 512,512 &
-#sleep 3
+export CUDA_VISIBLE_DEVICES=3
+python3 grid_search_pnbeats__exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 12 --singular_stack_num 1 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 &
+sleep 3
 
-#export CUDA_VISIBLE_DEVICES=4
-#python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 3 --n_block 1 --mlp_stack 512 &
-#sleep 3
-#
-#export CUDA_VISIBLE_DEVICES=5
-#python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 3 --n_block 1 --mlp_stack 512,512 &
-#sleep 3
-#
-#export CUDA_VISIBLE_DEVICES=6
-#python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 1 --n_block 1 --mlp_stack 512 &
-#sleep 3
-#
-#export CUDA_VISIBLE_DEVICES=7
-#python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 1 --n_block 1 --mlp_stack 512,512 &
-#sleep 3
+export CUDA_VISIBLE_DEVICES=4
+python3 grid_search_pnbeats__exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 15 --singular_stack_num 1 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 &
+sleep 3
+
+export CUDA_VISIBLE_DEVICES=5
+python3 grid_search_pnbeats__exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 18 --singular_stack_num 1 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 &
+sleep 3
+
+export CUDA_VISIBLE_DEVICES=6
+python3 grid_search_pnbeats__exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 21 --singular_stack_num 1 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 &
+sleep 3
+
+export CUDA_VISIBLE_DEVICES=7
+python3 grid_search_pnbeats__exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 24 --singular_stack_num 1 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 &
+sleep 3
---
+++ GSL/grid_search_pnbeats_exp.py
@@ -0,0 +1,66 @@
+import click
+from runner.runner import Runner
+import traceback
+from utils.logger import setup_logging
+import os
+from utils.train_helper import set_seed, mkdir, edict2dict
+import datetime
+import pytz
+from easydict import EasyDict as edict
+import yaml
+
+
+@click.command()
+@click.option('--conf_file_path', type=click.STRING, default=None)
+@click.option('--stack_num', type=int, default=1)
+@click.option('--singular_stack_num', type=int, default=1)
+@click.option('--n_pool_kernel_size', type=click.STRING, default='4')
+@click.option('--n_stride_size', type=click.STRING, default='2')
+def main(conf_file_path, stack_num, singular_stack_num, n_pool_kernel_size, n_stride_size):
+    temp = stack_num//3
+    n_pool_kernel_size = n_pool_kernel_size.split(',')
+    n_pool_kernel_size = [int(j.strip())for j in n_pool_kernel_size]
+    n_pool_kernel_size = n_pool_kernel_size * temp
+    n_pool_kernel_size.sort(reverse=True)
+
+    n_stride_size = n_stride_size.split(',')
+    n_stride_size = [int(jj.strip())for jj in n_stride_size]
+    n_stride_size = n_stride_size * temp
+    n_stride_size.sort(reverse=True)
+
+    hyperparameter = f'stacks_{stack_num}__singular_stack_num_{singular_stack_num}__n_pool_kernel_size_{n_pool_kernel_size}'
+
+    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+    sub_dir = '_'.join([hyperparameter, now.strftime('%m%d_%H%M%S')])
+    config = edict(yaml.load(open(conf_file_path, 'r'), Loader=yaml.FullLoader))
+    config.seed = set_seed(config.seed)
+
+    config.exp_dir = os.path.join(config.exp_dir, str(config.exp_name))
+    config.exp_sub_dir = os.path.join(config.exp_dir, sub_dir)
+    config.model_save = os.path.join(config.exp_sub_dir, "model_save")
+
+    mkdir(config.model_save)
+
+    config.forecasting_module.stack_num = stack_num
+    config.forecasting_module.singular_stack_num = singular_stack_num
+    config.forecasting_module.n_pool_kernel_size = n_pool_kernel_size
+    config.forecasting_module.n_stride_size = n_stride_size
+
+    save_name = os.path.join(config.exp_sub_dir, 'config.yaml')
+    yaml.dump(edict2dict(config), open(save_name, 'w'), default_flow_style=False)
+
+    log_file = os.path.join(config.exp_sub_dir, "log_exp_{}.txt".format(config.seed))
+    logger = setup_logging('INFO', log_file)
+    logger.info("Writing log file to {}".format(log_file))
+    logger.info("Exp instance id = {}".format(config.exp_name))
+
+    try:
+        runner = Runner(config=config)
+        runner.train()
+        runner.test()
+    except:
+        logger.error(traceback.format_exc())
+
+
+if __name__ == '__main__':
+    main()
