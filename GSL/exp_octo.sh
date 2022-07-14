72b210e5c7c6eeaa599eec28041fd404bb0e91f0
JuHyeon Kim <juhyeonkim@JuHyeonui-MacBookPro.local>
2022-07-15 00:11:45 +0900

    Octo EXP

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -4,33 +4,33 @@
 #sleep 240
 
 export CUDA_VISIBLE_DEVICES=0
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 512,512,512 &
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 1024 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 512,512 &
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 6 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 1024 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=2
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 96 &
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 9 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 1024 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=3
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 96,96 &
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 12 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 1024 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=4
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 96,96,96 &
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 15 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 1024 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=5
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 32 &
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 18 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 1024 &
 sleep 3
 
-#export CUDA_VISIBLE_DEVICES=6
-#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 32,32 &
-#sleep 3
-#
-#export CUDA_VISIBLE_DEVICES=7
-#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 3 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 32,32,32 &
-#sleep 3
+export CUDA_VISIBLE_DEVICES=6
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 21 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 1024 &
+sleep 3
+
+export CUDA_VISIBLE_DEVICES=7
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats.yaml --stack_num 24 --singular_stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --mlp_stack 1024 &
+sleep 3
