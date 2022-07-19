0be302f54a707ad5d4eb23bb779a1082c6978d8c
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-07-19 17:40:30 +0900

    Random Graph Exp @OCTO ECL

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -4,14 +4,14 @@
 #sleep 240
 
 #export CUDA_VISIBLE_DEVICES=0
-#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats_covid.yaml --stack_num 3 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
+#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats_covid.yaml --stack_num 3 --n_pool_kernel_size 16,8,4 --n_stride_size 4,4,2 &
 #sleep 3
 #python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats_covid_gl.yaml --stack_num 3 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
 #sleep 3
 
-#export CUDA_VISIBLE_DEVICES=1
-#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats_covid.yaml --stack_num 6 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
-#sleep 3
+export CUDA_VISIBLE_DEVICES=1
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/ECL/bk_loss_pn_beats_ecl.yaml --stack_num 12 --n_pool_kernel_size 16,8,4 --n_stride_size 4,4,2 &
+sleep 3
 #python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/pn_beats_covid_gl.yaml --stack_num 6 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
 #sleep 3
 
@@ -30,20 +30,20 @@
 
 
 
-#export CUDA_VISIBLE_DEVICES=4
-#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/bk_loss_pn_beats_covid.yaml --stack_num 3 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
-#sleep 3
+export CUDA_VISIBLE_DEVICES=4
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/ECL/bk_loss_pn_beats_ecl.yaml --stack_num 6 --n_pool_kernel_size 16,8,4 --n_stride_size 4,4,2 &
+sleep 3
 #python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/bk_loss_pn_beats_covid_gl.yaml --stack_num 3 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
 #sleep 3
 
-#export CUDA_VISIBLE_DEVICES=5
-#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/bk_loss_pn_beats_covid.yaml --stack_num 6 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
-#sleep 3
+export CUDA_VISIBLE_DEVICES=5
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/ECL/bk_loss_pn_beats_ecl.yaml --stack_num 3 --n_pool_kernel_size 16,8,4 --n_stride_size 4,4,2 &
+sleep 3
 #python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/bk_loss_pn_beats_covid_gl.yaml --stack_num 6 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
 #sleep 3
 
 export CUDA_VISIBLE_DEVICES=6
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/bk_loss_pn_beats_covid.yaml --stack_num 9 --n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/ECL/bk_loss_pn_beats_ecl.yaml --stack_num 9 --n_pool_kernel_size 16,8,4 --n_stride_size 4,4,2 &
 sleep 3
 #python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/bk_loss_pn_beats_covid_gl.yaml --stack_num 9 ---n_pool_kernel_size 1,1,1 --n_stride_size 1,1,1 &
 #sleep 3
