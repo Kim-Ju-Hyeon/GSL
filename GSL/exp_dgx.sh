c2d5fcc970e28a9a06cde103c329f1c76e600e8f
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-07-19 15:45:00 +0900

    Traffic Datasetis Too big to run at RTX3090 -> DGX

--- GSL/exp_dgx.sh
+++ GSL/exp_dgx.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=0
-python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 3 --n_block 3 --mlp_stack 64 --inter_correlation_stack_length 1 &
+export CUDA_VISIBLE_DEVICES=2
+python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/Traffic/bk_loss_pn_beats_traffic_gl.yaml --stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 &
 sleep 3
-#python3 grid_search_nbeats_I_exp.py --conf_file_path ./config/N_BEATS/nbeats_trend_season.yaml --n_stack 1 --n_block 3 --mlp_stack 64 --inter_correlation_stack_length 1 &
-#sleep 3
 
 
 #export CUDA_VISIBLE_DEVICES=1
--- GSL/exp_rtx90.sh
+++ GSL/exp_rtx90.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 
-export CUDA_VISIBLE_DEVICES=0
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/ETT/bk_loss_pn_beats_ett_gl.yaml --stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 &
-sleep 3
-
-export CUDA_VISIBLE_DEVICES=1
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/Exchange/bk_loss_pn_beats_exchange_gl.yaml --stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 &
-sleep 3
+#export CUDA_VISIBLE_DEVICES=0
+#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/ETT/bk_loss_pn_beats_ett_gl.yaml --stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 &
+#sleep 3
+#
+#export CUDA_VISIBLE_DEVICES=1
+#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/Exchange/bk_loss_pn_beats_exchange_gl.yaml --stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 &
+#sleep 3
 
 export CUDA_VISIBLE_DEVICES=2
 python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/Traffic/bk_loss_pn_beats_traffic_gl.yaml --stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 &
 sleep 3
 
-export CUDA_VISIBLE_DEVICES=3
-python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/Weather/bk_loss_pn_beats_wth_gl.yaml --stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 &
-sleep 3
+#export CUDA_VISIBLE_DEVICES=3
+#python3 grid_search_pnbeats_exp.py --conf_file_path ./config/PN_BEATS/Weather/bk_loss_pn_beats_wth_gl.yaml --stack_num 3 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 &
+#sleep 3
