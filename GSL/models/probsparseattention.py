a5737bc9585c924616babe9d10614e4950e598d8
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-07-25 18:04:13 +0900

    Prob Sparse Attention

--- GSL/models/probsparseattention.py
+++ GSL/models/probsparseattention.py
@@ -8,27 +8,23 @@ from math import sqrt
 
 
 class ProbAttention(nn.Module):
-    def __init__(self, mask_flag=True, factor=5, scale=None, attention_dropout=0.1, output_attention=False):
+    def __init__(self, factor):
         super(ProbAttention, self).__init__()
+
         self.factor = factor
-        self.scale = scale
-        self.mask_flag = mask_flag
-        self.output_attention = output_attention
-        self.dropout = nn.Dropout(attention_dropout)
 
     def _prob_QK(self, Q, K, sample_k, n_top):  # n_top: c*ln(L_q)
-        # Q [B, H, L, D]
-        B, H, L_K, E = K.shape
-        _, _, L_Q, _ = Q.shape
+        B, H, L, E = K.shape
+        _, _, L, _ = Q.shape
 
         # calculate the sampled Q_K
-        K_expand = K.unsqueeze(-3).expand(B, H, L_Q, L_K, E)
-        index_sample = torch.randint(L_K, (L_Q, sample_k))  # real U = U_part(factor*ln(L_k))*L_q
-        K_sample = K_expand[:, :, torch.arange(L_Q).unsqueeze(1), index_sample, :]
+        K_expand = K.unsqueeze(-3).expand(B, H, L, L, E)
+        index_sample = torch.randint(L, (L, sample_k))  # real U = U_part(factor*ln(L_k))*L_q
+        K_sample = K_expand[:, :, torch.arange(L).unsqueeze(1), index_sample, :]
         Q_K_sample = torch.matmul(Q.unsqueeze(-2), K_sample.transpose(-2, -1)).squeeze(-2)
 
         # find the Top_k query with sparisty measurement
-        M = Q_K_sample.max(-1)[0] - torch.div(Q_K_sample.sum(-1), L_K)
+        M = Q_K_sample.max(-1)[0] - torch.div(Q_K_sample.sum(-1), L)
         M_top = M.topk(n_top, sorted=False)[1]
 
         # use the reduced Q to calculate Q_K
@@ -39,72 +35,47 @@ class ProbAttention(nn.Module):
 
         return Q_K, M_top
 
-    def _get_initial_context(self, V, L_Q):
-        B, H, L_V, D = V.shape
-        if not self.mask_flag:
-            # V_sum = V.sum(dim=-2)
-            V_sum = V.mean(dim=-2)
-            contex = V_sum.unsqueeze(-2).expand(B, H, L_Q, V_sum.shape[-1]).clone()
-        else:  # use mask
-            assert (L_Q == L_V)  # requires that L_Q == L_V, i.e. for self-attention only
-            contex = V.cumsum(dim=-2)
-        return contex
-
-    def _update_context(self, context_in, V, scores, index, L_Q, attn_mask):
-        B, H, L_V, D = V.shape
-
-        if self.mask_flag:
-            attn_mask = ProbMask(B, H, L_Q, index, scores, device=V.device)
-            scores.masked_fill_(attn_mask.mask, -np.inf)
-
+    def _update_context(self, init_attn, scores, index):
+        B, H, _, _ = init_attn.shape
         attn = torch.softmax(scores, dim=-1)  # nn.Softmax(dim=-1)(scores)
 
-        context_in[torch.arange(B)[:, None, None],
-        torch.arange(H)[None, :, None],
-        index, :] = torch.matmul(attn, V).type_as(context_in)
-        if self.output_attention:
-            attns = (torch.ones([B, H, L_V, L_V]) / L_V).type_as(attn).to(attn.device)
-            attns[torch.arange(B)[:, None, None], torch.arange(H)[None, :, None], index, :] = attn
-            return (context_in, attns)
-        else:
-            return (context_in, None)
+        init_attn[torch.arange(B)[:, None, None], torch.arange(H)[None, :, None], index, :] = attn
 
-    def forward(self, queries, keys, values, attn_mask):
-        B, L_Q, H, D = queries.shape
-        _, L_K, _, _ = keys.shape
+        return init_attn
+
+    def forward(self, queries, keys):
+        B, L, H, D = queries.shape
 
         queries = queries.transpose(2, 1)
         keys = keys.transpose(2, 1)
-        values = values.transpose(2, 1)
 
-        U_part = self.factor * np.ceil(np.log(L_K)).astype('int').item()  # c*ln(L_k)
-        u = self.factor * np.ceil(np.log(L_Q)).astype('int').item()  # c*ln(L_q)
+        U_part = self.factor * np.ceil(np.log(L)).astype('int').item()  # c*ln(L_k)
+        u = self.factor * np.ceil(np.log(L)).astype('int').item()  # c*ln(L_q)
 
-        U_part = U_part if U_part < L_K else L_K
-        u = u if u < L_Q else L_Q
+        U_part = U_part if U_part < L else L
+        u = u if u < L else L
 
         scores_top, index = self._prob_QK(queries, keys, sample_k=U_part, n_top=u)
 
         # add scale factor
-        scale = self.scale or 1. / sqrt(D)
-        if scale is not None:
-            scores_top = scores_top * scale
+        scale = 1. / sqrt(D)
+        scores_top = scores_top * scale
 
-
-        # get the context
-        context = self._get_initial_context(values, L_Q)
         # update the context with selected top_k queries
-        context, attn = self._update_context(context, values, scores_top, index, L_Q, attn_mask)
+        attn = torch.softmax(scores_top, dim=-1)
 
-        return context.transpose(2, 1).contiguous(), attn
+        attention_matirx = (torch.ones([B, H, L, L]) / L).type_as(attn).to(scores_top.device)
+        attention_matirx[torch.arange(B)[:, None, None], torch.arange(H)[None, :, None], index, :] = attn
+        return attention_matirx
+
 
 class AttentionLayer(nn.Module):
     def __init__(self, attention, d_model, n_heads,
                  d_keys=None, d_values=None, mix=False):
         super(AttentionLayer, self).__init__()
 
-        d_keys = d_keys or (d_model//n_heads)
-        d_values = d_values or (d_model//n_heads)
+        d_keys = d_keys or (d_model // n_heads)
+        d_values = d_values or (d_model // n_heads)
 
         self.inner_attention = attention
         self.query_projection = nn.Linear(d_model, d_keys * n_heads)
@@ -130,9 +101,43 @@ class AttentionLayer(nn.Module):
             attn_mask
         )
         if self.mix:
-            out = out.transpose(2,1).contiguous()
+            out = out.transpose(2, 1).contiguous()
         out = out.view(B, L, -1)
 
         return self.out_projection(out), attn
 
 
+class GraphLearningProbSparseAttention(nn.Module):
+    def __init__(self, n_head, d_model, num_nodes, dropout_rate=0.5, factor=2):
+        super(GraphLearningProbSparseAttention, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.num_nodes = num_nodes
+
+        self.d_k = self.d_q = d_model // n_head
+        self.dropout = nn.Dropout(p=dropout_rate)
+
+        self.query_projection = nn.Linear(self.d_model, self.d_k * self.n_head)
+        self.key_projection = nn.Linear(self.d_model, self.d_k * self.n_head)
+
+        self.attention = ProbAttention(factor)
+
+        self.init_weights()
+
+    def init_weights(self):
+        for name, p in self.named_parameters():
+            if 'bias' not in name:
+                torch.nn.init.xavier_uniform_(p)
+            else:
+                torch.nn.init.zeros_(p)
+
+    def forward(self, q, k):
+        B, nodes_num, hidden = q.shape
+
+        queries = self.query_projection(q).view(B, nodes_num, self.n_head, -1)
+        keys = self.key_projection(k).view(B, nodes_num, self.n_head, -1)
+
+        attn = self.attention(queries, keys)
+
+        return attn
