6aa94595c3357b35c9479ce5ad19d1138abad1ef
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-07-26 16:30:05 +0900

    Prob Sparse Attention For GL -> Multi Head Structure

--- GSL/models/probsparseattention.py
+++ GSL/models/probsparseattention.py
@@ -66,45 +66,8 @@ class ProbAttention(nn.Module):
 
         attention_matirx = (torch.ones([B, H, L, L]) / L).type_as(attn).to(scores_top.device)
         attention_matirx[torch.arange(B)[:, None, None], torch.arange(H)[None, :, None], index, :] = attn
+
         return attention_matirx
-
-
-class AttentionLayer(nn.Module):
-    def __init__(self, attention, d_model, n_heads,
-                 d_keys=None, d_values=None, mix=False):
-        super(AttentionLayer, self).__init__()
-
-        d_keys = d_keys or (d_model // n_heads)
-        d_values = d_values or (d_model // n_heads)
-
-        self.inner_attention = attention
-        self.query_projection = nn.Linear(d_model, d_keys * n_heads)
-        self.key_projection = nn.Linear(d_model, d_keys * n_heads)
-        self.value_projection = nn.Linear(d_model, d_values * n_heads)
-        self.out_projection = nn.Linear(d_values * n_heads, d_model)
-        self.n_heads = n_heads
-        self.mix = mix
-
-    def forward(self, queries, keys, values, attn_mask):
-        B, L, _ = queries.shape
-        _, S, _ = keys.shape
-        H = self.n_heads
-
-        queries = self.query_projection(queries).view(B, L, H, -1)
-        keys = self.key_projection(keys).view(B, S, H, -1)
-        values = self.value_projection(values).view(B, S, H, -1)
-
-        out, attn = self.inner_attention(
-            queries,
-            keys,
-            values,
-            attn_mask
-        )
-        if self.mix:
-            out = out.transpose(2, 1).contiguous()
-        out = out.view(B, L, -1)
-
-        return self.out_projection(out), attn
 
 
 class GraphLearningProbSparseAttention(nn.Module):
