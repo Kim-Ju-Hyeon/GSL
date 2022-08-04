1ff7e08c2f195b88fc876657e179a5621853c72d
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-08-04 15:17:05 +0900

    ETTm2 Exp

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -2,15 +2,15 @@
 
 
 export CUDA_VISIBLE_DEVICES=0
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 3 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 --factor 2 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 3 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 --factor 4 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 3 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 --factor 3 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 --factor 4 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=2
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 --factor 2 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 9 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 --factor 4 &
 sleep 3
 
 #export CUDA_VISIBLE_DEVICES=3
@@ -21,13 +21,13 @@ sleep 3
 #python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --factor 2 &
 #sleep 3
 
-export CUDA_VISIBLE_DEVICES=5
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2--factor 3 &
-sleep 3
-
-export CUDA_VISIBLE_DEVICES=6
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 9 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 --factor 2 &
-sleep 3
+#export CUDA_VISIBLE_DEVICES=5
+#python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2--factor 4 &
+#sleep 3
+#
+#export CUDA_VISIBLE_DEVICES=6
+#python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 9 --n_pool_kernel_size 16,8,4 --n_stride_size 8,4,2 --factor 4 &
+#sleep 3
 
 #export CUDA_VISIBLE_DEVICES=7
 #python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 9 --n_pool_kernel_size 8,4,2 --n_stride_size 4,2,1 --factor 1 &
