13d95fbe4de3d69cf97d0dd9ff509ed6145973ed
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-08-12 16:27:19 +0900

    Univariate and Multivariate Dataset and make DataLoader Efficiently and dontt save dataset

--- GSL/dataset/temporal_graph_dataset.py
+++ GSL/dataset/temporal_graph_dataset.py
@@ -1,7 +1,10 @@
 import numpy as np
 import random
 import torch
-from torch_geometric_temporal.signal import StaticGraphTemporalSignalBatch
+
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+
 from utils.scalers import Scaler
 import os
 import pandas as pd
@@ -14,64 +17,65 @@ from utils.dataset_utils import time_features_from_frequency_str
 
 
 class Temporal_Graph_Signal(object):
-    def __init__(self, dataset_name, scaler_type='std'):
+    def __init__(self, dataset_name, scaler_type='std', univariate=False):
         super(Temporal_Graph_Signal, self).__init__()
         self.dataset_name = dataset_name
+        self.univariate = univariate
         self._set_dataset_parameters(dataset_name)
         self.scaler = Scaler(scaler_type)
 
     def preprocess_dataset(self):
-            if (self.dataset_name == 'METR-LA') or (self.dataset_name == 'PEMS-BAY'):
-                X, A = self._get_torch_geometric_dataset()
+        if (self.dataset_name == 'METR-LA') or (self.dataset_name == 'PEMS-BAY'):
+            X, A = self._get_torch_geometric_dataset()
+            X = X.astype(np.float32)
+            X = X.transpose((1, 2, 0))
+            X = self.scaler.scale(X)
+
+            total_sequence_length = X.shape[-1]
+            train_index = int(total_sequence_length * 0.7) + 1
+            valid_index = int(total_sequence_length * 0.2) + 1 + train_index
+
+            self.train_X = X[:, :, :train_index]
+            self.valid_X = X[:, :, train_index:valid_index]
+            self.test_X = X[:, :, valid_index:]
+
+            self.A = torch.from_numpy(A)
+        else:
+            self._read_web_data()
+
+            y_df = pd.read_csv(os.path.join(self.path, f'{self.dataset_name}.csv'), index_col=self.index_col)
+
+            if self.dataset_name == 'Exchange':
+                X = y_df.to_numpy().T
                 X = X.astype(np.float32)
-                X = X.transpose((1, 2, 0))
+                X = self.scaler.scale(X)
+            else:
+                _df = y_df.drop(['date'], axis=1).to_numpy().T
+                X = _df.astype(np.float32)
                 X = self.scaler.scale(X)
 
-                total_sequence_length = X.shape[-1]
-                train_index = int(total_sequence_length * 0.7) + 1
-                valid_index = int(total_sequence_length * 0.2) + 1 + train_index
+                if not self.univariate:
+                    self.time_stamp = self._get_timestamp(y_df)
 
-                self.train_X = X[:, :, :train_index]
-                self.valid_X = X[:, :, train_index:valid_index]
-                self.test_X = X[:, :, valid_index:]
+            total_sequence_length = X.shape[-1]
+            train_index = int(total_sequence_length * 0.6) + 1
+            valid_index = int(total_sequence_length * 0.2) + 1 + train_index
 
-                self.A = torch.from_numpy(A)
-            else:
-                self._read_web_data()
+            self.train_X = X[:, :, :train_index]
+            self.valid_X = X[:, :, train_index:valid_index]
+            self.test_X = X[:, :, valid_index:]
 
-                y_df = pd.read_csv(os.path.join(self.path, f'{self.dataset_name}.csv'), index_col=self.index_col)
+    def _get_timestamp(self, y_df):
+        y_df['date'] = pd.to_datetime(y_df['date'])
 
-                if self.dataset_name == 'Exchange':
-                    X = y_df.to_numpy().T
-                    X = np.expand_dims(X, axis=1)
-                    X = self.scaler.scale(X)
-                else:
-                    y_df['date'] = pd.to_datetime(y_df['date'])
-                    y_df.rename(columns={'date': 'ds'}, inplace=True)
-                    u_ids = y_df.columns.to_list()
-                    u_ids.remove('ds')
-                    time_cls = time_features_from_frequency_str(self.freq)
-                    for cls_ in time_cls:
-                        cls_name = cls_.__class__.__name__
-                        y_df[cls_name] = cls_(y_df['ds'].dt)
+        dataframe = pd.DataFrame()
+        time_cls = time_features_from_frequency_str('1H')
+        for cls_ in time_cls:
+            cls_name = cls_.__class__.__name__
+            dataframe[cls_name] = cls_(y_df['date'].dt)
+        time_stamp = dataframe.to_numpy().T
 
-                    time_stamp = y_df.drop(u_ids + ['ds'], axis=1).to_numpy().T
-                    temp = np.array([time_stamp for _ in range(self.nodes_num)])
-
-                    df = y_df[u_ids].to_numpy().T
-                    df = np.expand_dims(df, axis=1)
-                    df = self.scaler.scale(df)
-
-                    # Total X dimension = [Number of Nodes, Number of Features, Sequence Length]
-                    X = np.concatenate([df, temp], axis=1)
-
-                total_sequence_length = X.shape[-1]
-                train_index = int(total_sequence_length * 0.6) + 1
-                valid_index = int(total_sequence_length * 0.2) + 1 + train_index
-
-                self.train_X = X[:, :, :train_index]
-                self.valid_X = X[:, :, train_index:valid_index]
-                self.test_X = X[:, :, valid_index:]
+        return time_stamp
 
     def _set_dataset_parameters(self, dataset_name):
         ett_dataset_list = ['ETTm1', 'ETTm2', 'ETTh1', 'ETTh2']
@@ -80,6 +84,7 @@ class Temporal_Graph_Signal(object):
             self.nodes_num = 207
             self.node_features = 2
             self.url = "https://graphmining.ai/temporal_datasets/METR-LA.zip"
+
         elif dataset_name == 'PEMS-BAY':
             self.nodes_num = 325
             self.node_features = 2
@@ -91,12 +96,14 @@ class Temporal_Graph_Signal(object):
             self.node_features = 4
             self.freq = '1D'
             self.url = 'https://drive.google.com/file/d/1rPwzpCH8fzNiteXMyO71EbKixievDu3j/view?usp=sharing'
+
         elif self.dataset_name == 'ECL':
             self.index_col = False
             self.nodes_num = 321
             self.node_features = 5
             self.url = 'https://drive.google.com/file/d/1nzq4Q3bdVHBqpiz4V7hR1Z3w-kssADmt/view?usp=sharing'
             self.freq = '1H'
+
         elif dataset_name in ett_dataset_list:
             self.index_col = False
             self.nodes_num = 7
@@ -208,18 +215,11 @@ class Temporal_Graph_Signal(object):
 
         return X, A
 
-    def _generate_dataset(self, dataset, num_timesteps_in: int = 12, num_timesteps_out: int = 12,
-                          batch_size: int = 32,
-                          inference: bool = False):
+    def _generate_dataset(self, dataset, num_timesteps_in: int = 12, num_timesteps_out: int = 12):
         indices = [
             (i, i + (num_timesteps_in + num_timesteps_out))
             for i in range(dataset.shape[2] - (num_timesteps_in + num_timesteps_out) + 1)
         ]
-
-        if inference:
-            pass
-        else:
-            random.shuffle(indices)
 
         features, target = [], []
         for i, j in indices:
@@ -229,39 +229,25 @@ class Temporal_Graph_Signal(object):
         features = torch.FloatTensor(np.array(features))
         targets = torch.FloatTensor(np.array(target))
 
-        dataset = self._get_batch_graph_temporal_signal_batch(num_timesteps_in, num_timesteps_out,
-                                                              features, targets, batch_size)
+        _data = []
 
-        return dataset
+        for batch in range(len(indices)):
+            _data.append(Data(x=features[batch], y=targets[batch]))
 
-    def _get_batch_graph_temporal_signal_batch(self, num_timesteps_in, num_timesteps_out, features, targets,
-                                               batch_size):
-        data_batch = features.shape[0] // batch_size
-        node_num, feature_dim = features.shape[1], features.shape[2]
-
-        _batch = []
-        for batch_iter in range(batch_size):
-            _batch += [batch_iter] * node_num
-        _batch = np.array(_batch)
-
-        feature_Tensor = features[:data_batch * batch_size].reshape(data_batch, node_num * batch_size,
-                                                                    feature_dim, num_timesteps_in).numpy()
-
-        target_Tensor = targets[:data_batch * batch_size].reshape(data_batch, node_num * batch_size,
-                                                                  num_timesteps_out).numpy()
-
-        dataset = StaticGraphTemporalSignalBatch(edge_index=None, edge_weight=None,
-                                                 features=feature_Tensor, targets=target_Tensor, batches=_batch)
-
-        return dataset
+        return _data
 
     def get_dataset(self, num_timesteps_in: int = 12, num_timesteps_out: int = 12, batch_size: int = 32):
-        train_dataset = self._generate_dataset(self.train_X, num_timesteps_in, num_timesteps_out, batch_size)
-        valid_dataset = self._generate_dataset(self.valid_X, num_timesteps_in, num_timesteps_out, batch_size)
-        test_dataset = self._generate_dataset(self.test_X, num_timesteps_in, num_timesteps_out, 1,
-                                              inference=True)
+        train_dataset = self._generate_dataset(self.train_X, num_timesteps_in, num_timesteps_out)
+        train = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
-        return train_dataset, valid_dataset, test_dataset
+        valid_dataset = self._generate_dataset(self.valid_X, num_timesteps_in, num_timesteps_out)
+        valid = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+        test_dataset = self._generate_dataset(self.test_X, num_timesteps_in, num_timesteps_out)
+        test = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+        return train, valid, test
 
     def get_scaler(self):
         return self.scaler
+
