cbe3e9421a7e23ca7a4425becc28237e7beb1be2
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-08-17 11:36:59 +0900

    Train Reusme

--- GSL/run_inference.py
+++ GSL/run_inference.py
@@ -9,15 +9,22 @@ import yaml
 
 @click.command()
 @click.option('--conf_file_path', type=click.STRING, default=None)
-def main(conf_file_path):
+@click.option('--inference', type=bool, default=True)
+def main(conf_file_path, inference):
+    if inference:
+        log_save_name = 'Inference_log_exp'
+    else:
+        log_save_name = 'Train_Resume'
     config = edict(yaml.load(open(conf_file_path, 'r'), Loader=yaml.FullLoader))
-    log_file = os.path.join(config.exp_sub_dir, "Inference_log_exp_{}.txt".format(config.seed))
+    log_file = os.path.join(config.exp_sub_dir, f"{log_save_name}_log_exp_{config.seed}.txt")
     logger = setup_logging('INFO', log_file, logger_name=str(config.seed))
     logger.info("Writing log file to {}".format(log_file))
     logger.info("Exp instance id = {}".format(config.exp_name))
 
     try:
         runner = Runner(config=config)
+        if not inference:
+            runner.train()
         runner.test()
     except:
         logger.error(traceback.format_exc())
