cb5edc4abe57edeb8d9fbf88f76e828701c79f7c
KimJuHyeon <wngus1310@naver.com>
2022-08-19 03:11:25 +0900

    EXP Exchange

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -2,33 +2,33 @@
 
 
 export CUDA_VISIBLE_DEVICES=0
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 1 --singular_stack 1 --mlp_stack 32 --mlp_num 1 --thetas_dim 32,32 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 1 --mlp_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 1 --singular_stack 1 --mlp_stack 32 --mlp_num 3 --thetas_dim 32,32 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 1 --mlp_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=2
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 1 --singular_stack 3 --mlp_stack 32 --mlp_num 1 --thetas_dim 32,32 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 3 --mlp_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=3
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 1 --singular_stack 3 --mlp_stack 32 --mlp_num 3 --thetas_dim 32,32 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 3 --mlp_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=4
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --singular_stack 1 --mlp_stack 32 --mlp_num 1 --thetas_dim 32,32 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 5 --mlp_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=5
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --singular_stack 1 --mlp_stack 32 --mlp_num 3 --thetas_dim 32,32 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 5 --mlp_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=6
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --singular_stack 3 --mlp_stack 32 --mlp_num 1 --thetas_dim 32,32 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 10 --mlp_num 1 &
 sleep 3
 #
 export CUDA_VISIBLE_DEVICES=7
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ettm2.yaml --stack_num 6 --singular_stack 3 --mlp_stack 32 --mlp_num 3 --thetas_dim 32,32 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 10 --mlp_num 1 &
 sleep 3
--- GSL/run_ic_pnbeats_exp.py
+++ GSL/run_ic_pnbeats_exp.py
@@ -17,71 +17,79 @@ import yaml
 @click.option('--singular_stack', type=int, default=1)
 @click.option('--mlp_stack', type=int, default=1)
 @click.option('--mlp_num', type=int, default=1)
-@click.option('--thetas_dim', type=click.STRING, default='64,64')
-def main(conf_file_path, stack_num, singular_stack, mlp_stack, mlp_num, thetas_dim):
-    thetas_dim = thetas_dim.split(',')
-    thetas_dim = [int(j.strip()) for j in thetas_dim]
+def main(conf_file_path, stack_num, singular_stack, mlp_stack, mlp_num):
+    mlp_stack_list = [32, 256, 512, 1024]
+    singular_stack_num_list = [1, 3]
 
-    config = edict(yaml.load(open(conf_file_path, 'r'), Loader=yaml.FullLoader))
+    for singular_stack_num in singular_stack_num_list:
+        for mlp_stack in mlp_stack_list:
+            config = edict(yaml.load(open(conf_file_path, 'r'), Loader=yaml.FullLoader))
 
-    hyperparameter = f'stacks_{stack_num}' \
-                     f'__mlp_stack_{mlp_stack}*{mlp_num}' \
-                     f'__singular_stack_{singular_stack}' \
-                     f'__thetas_dim_{thetas_dim}'
+            hyperparameter = f'stacks_{stack_num}' \
+                             f'__mlp_stack_{mlp_stack}*{mlp_num}' \
+                             f'__singular_stack_{singular_stack_num}'
 
-    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
-    sub_dir = '_'.join([hyperparameter, now.strftime('%m%d_%H%M%S')])
-    config.seed = set_seed(config.seed)
+            now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+            sub_dir = '_'.join([hyperparameter, now.strftime('%m%d_%H%M%S')])
+            config.seed = set_seed(config.seed)
 
-    config.exp_name = config.exp_name
+            config.exp_name = config.exp_name
 
-    config.exp_dir = os.path.join(config.exp_dir, str(config.exp_name))
-    config.exp_sub_dir = os.path.join(config.exp_dir, sub_dir)
-    config.model_save = os.path.join(config.exp_sub_dir, "model_save")
+            config.exp_dir = os.path.join(config.exp_dir, str(config.exp_name))
+            config.exp_sub_dir = os.path.join(config.exp_dir, sub_dir)
+            config.model_save = os.path.join(config.exp_sub_dir, "model_save")
 
-    mkdir(config.model_save)
+            mkdir(config.model_save)
 
-    if config.dataset.name == 'ECL':
-        if stack_num == 1:
-            config.train.batch_size = 16
-        elif stack_num == 2:
-            config.train.batch_size = 16
-        elif stack_num == 3:
-            config.train.batch_size = 8
+            if config.dataset.name == 'ECL':
+                if stack_num == 1:
+                    config.train.batch_size = 16
+                elif stack_num == 2:
+                    config.train.batch_size = 16
+                elif stack_num == 3:
+                    config.train.batch_size = 8
 
-    if stack_num == 1:
-        n_pool_kernel_size = [8]
-        n_stride_size = [4]
-    elif stack_num == 2:
-        n_pool_kernel_size = [8, 4]
-        n_stride_size = [4, 2]
-    elif stack_num == 3:
-        n_pool_kernel_size = [8, 4, 2]
-        n_stride_size = [4, 2, 1]
+            if stack_num == 1:
+                n_pool_kernel_size = [8]
+                n_stride_size = [4]
+            elif stack_num == 2:
+                n_pool_kernel_size = [8, 4]
+                n_stride_size = [4, 2]
+            elif stack_num == 3:
+                n_pool_kernel_size = [8, 4, 2]
+                n_stride_size = [4, 2, 1]
+            elif stack_num == 4:
+                n_pool_kernel_size = [8, 4, 4, 2]
+                n_stride_size = [4, 2, 2, 1]
+            elif stack_num == 5:
+                n_pool_kernel_size = [8, 4, 4, 2, 2]
+                n_stride_size = [4, 2, 2, 1, 1]
+            elif stack_num == 10:
+                n_pool_kernel_size = [8, 8, 4, 4, 4, 4, 2, 2, 2, 2]
+                n_stride_size = [4, 4, 2, 2, 2, 2, 1, 1, 1, 1]
 
-    config.forecasting_module.stack_num = stack_num
-    config.forecasting_module.singular_stack_num = singular_stack
-    config.forecasting_module.n_theta_hidden = [mlp_stack] * mlp_num
-    config.forecasting_module.thetas_dim = thetas_dim
+            config.forecasting_module.stack_num = stack_num
+            config.forecasting_module.singular_stack_num = singular_stack
+            config.forecasting_module.n_theta_hidden = [mlp_stack] * mlp_num
 
-    config.forecasting_module.n_pool_kernel_size = n_pool_kernel_size
-    config.forecasting_module.n_stride_size = n_stride_size
+            config.forecasting_module.n_pool_kernel_size = n_pool_kernel_size
+            config.forecasting_module.n_stride_size = n_stride_size
 
-    save_name = os.path.join(config.exp_sub_dir, 'config.yaml')
-    yaml.dump(edict2dict(config), open(save_name, 'w'), default_flow_style=False)
+            save_name = os.path.join(config.exp_sub_dir, 'config.yaml')
+            yaml.dump(edict2dict(config), open(save_name, 'w'), default_flow_style=False)
 
-    log_file = os.path.join(config.exp_sub_dir, "log_exp_{}.txt".format(config.seed))
-    logger = setup_logging('INFO', log_file, logger_name=str(config.seed))
-    logger.info("Writing log file to {}".format(log_file))
-    logger.info("Exp instance id = {}".format(config.exp_name))
+            log_file = os.path.join(config.exp_sub_dir, "log_exp_{}.txt".format(config.seed))
+            logger = setup_logging('INFO', log_file, logger_name=str(config.seed))
+            logger.info("Writing log file to {}".format(log_file))
+            logger.info("Exp instance id = {}".format(config.exp_name))
 
-    try:
-        runner = Runner(config=config)
-        runner.train()
-        runner.test()
+            try:
+                runner = Runner(config=config)
+                runner.train()
+                runner.test()
 
-    except:
-        logger.error(traceback.format_exc())
+            except:
+                logger.error(traceback.format_exc())
 
 
 if __name__ == '__main__':
