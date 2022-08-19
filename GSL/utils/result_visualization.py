dc0601e3f9d2c7ebf6448d4a67d215eec324eb90
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-08-19 13:44:07 +0900

    Test Dataset Loader

--- GSL/utils/result_visualization.py
+++ GSL/utils/result_visualization.py
@@ -5,6 +5,7 @@ from easydict import EasyDict as edict
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 from torch_geometric.utils import to_dense_adj, dense_to_sparse
 
@@ -28,7 +29,11 @@ def get_exp_result_files(exp):
     return config, train_result, test_result
 
 
-def get_scaler_and_test_dataset(dataset):
+def get_scaler_and_test_dataset(config):
+    dataset = config.dataset.name
+    num_timesteps_in = config.forecasting_module.backcast_length
+    num_timesteps_out = config.forecasting_module.forecast_length
+
     _dir = f'../GSL/data/{dataset}'
 
     ett_dataset_list = ['ETTm1', 'ETTm2', 'ETTh1', 'ETTh2']
@@ -42,7 +47,20 @@ def get_scaler_and_test_dataset(dataset):
         open(os.path.join(_dir, 'inference.pickle'), 'rb')
     )
 
-    return scaler, test_dataset
+    indices = [
+        (i, i + (num_timesteps_in + num_timesteps_out))
+        for i in range(test_dataset.shape[2] - (num_timesteps_in + num_timesteps_out) + 1)
+    ]
+
+    features, target = [], []
+    for i, j in indices:
+        features.append((test_dataset[:, 0, i: i + num_timesteps_in]))
+        target.append((test_dataset[:, 0, i + num_timesteps_in: j]))
+
+    features = torch.FloatTensor(np.array(features))
+    targets = torch.FloatTensor(np.array(target))
+
+    return scaler, features, targets
 
 
 # def visualize_inference_result(test_result):
@@ -52,98 +70,98 @@ def get_scaler_and_test_dataset(dataset):
 #     inputs = test_result['Inputs']
 #     backcast = test_result['backcast']
 
-class visualize_inference_result:
-    def __init__(self, exp):
-        self.config, self.train_result, self.test_result = get_exp_result_files(exp)
-        self.scaler = get_scaler(self.config)
-
-        self._inference_result(self.test_result)
-
-    def _inference_result(self, test_result):
-        self.target = test_result['target']
-        self.pred = test_result['prediction']
-
-        self.inputs = test_result['Inputs']
-        self.backcast = test_result['backcast']
-
-        if self.config.forecasting_module.name == 'pn_beats':
-            self.per_trend_backcast = test_result['per_trend_backcast']
-            self.per_trend_forecast = test_result['per_trend_forecast']
-
-            self.per_seasonality_backcast = test_result['per_seasonality_backcast']
-            self.per_seasonality_forecast = test_result['per_seasonality_forecast']
-
-            self.singual_backcast = test_result['singual_backcast']
-            self.singual_forecast = test_result['singual_forecast']
-
-        elif self.config.forecasting_module.name == 'n_beats':
-            self.stack_per_forecast = np.stack(test_result['stack_per_forecast'])
-            self.stack_per_backcast = np.stack(test_result['stack_per_backcast'])
-
-            self.block_per_backcast = test_result['block_per_backcast']
-            self.block_per_forecast = test_result['block_per_forecast']
-
-    def visualize_val_adj_per_epoch(self):
-        edge_ = []
-
-        for i in range(self.config.train.epoch):
-            edge_.append(dense_to_sparse(self.train_result['val_adj_matirix'][i])[0].shape[1])
-
-        f, axes = plt.subplots(figsize=(10, 5))
-
-        axes.plot(edge_, label='Learned Edge')
-        axes.set_xlabel('Epoch')
-        axes.set_ylabel('# of Total Edge')
-        axes.legend()
-
-    def visualize_test_adj_matrix(self):
-        learn_adj = self.test_result['adj_matrix']
-
-        f, axes = plt.subplots(figsize=(10, 10))
-
-        axes.imshow(learn_adj, cmap='Greys')
-
-    def visualize_learning_curve(self):
-        f, axes = plt.subplots(figsize=(10, 5))
-
-        axes.plot(self.train_result['train_loss'], label='Train Loss')
-        axes.plot(self.train_result['val_loss'], label='Validation Loss')
-        axes.set_xlabel('Epoch')
-        axes.set_ylabel('Loss')
-        axes.legend()
-
-    def visualize_node(self, node: int = -2, figure_num: int = 5, save: bool = False, backcast: bool = False):
-        nrow = figure_num
-        ncol = 1
-
-        input_length = self.config.forecasting_module.backcast_length
-        output_length = self.config.forecasting_module.forecast_length
-
-        x_axis = np.arange(input_length + output_length)
-
-        f, axes = plt.subplots(nrows=nrow, ncols=ncol, figsize=(4 * nrow, 50 * ncol), dpi=70)
-
-        for ii in range(nrow):
-            axes[ii].plot(x_axis[:input_length], self.inputs[ii, node, 0], label='Input')
-
-            if backcast:
-                axes[ii].plot(x_axis[:input_length], self.backcast[ii, node], label='Backcast')
-
-            axes[ii].plot(x_axis[input_length:], self.target[ii, node], label='Target')
-            axes[ii].plot(x_axis[input_length:], self.pred[ii, node], label='Prediction')
-            axes[ii].legend()
-
-        if save:
-            f.savefig('./total_result.png')
-
-    def visualize_stack_output(self, node: int = -2, save: bool = False):
-        nrow = self.per_trend_backcast.shape[1] + self.singual_backcast.shape[1]
-        ncol = 2
-
-        input_length = self.config.forecasting_module.backcast_length
-        output_length = self.config.forecasting_module.forecast_length
-
-        x_axis = np.arange(input_length + output_length)
-
-        if self.config.forecasting_module.name == 'pn_beats':
-            f, axes = plt.subplots(nrows=nrow, ncols=ncol, figsize=(4 * nrow, 50 * ncol), dpi=70)
+# class visualize_inference_result:
+#     def __init__(self, exp):
+#         self.config, self.train_result, self.test_result = get_exp_result_files(exp)
+#         self.scaler = get_scaler(self.config)
+#
+#         self._inference_result(self.test_result)
+#
+#     def _inference_result(self, test_result):
+#         self.target = test_result['target']
+#         self.pred = test_result['prediction']
+#
+#         self.inputs = test_result['Inputs']
+#         self.backcast = test_result['backcast']
+#
+#         if self.config.forecasting_module.name == 'pn_beats':
+#             self.per_trend_backcast = test_result['per_trend_backcast']
+#             self.per_trend_forecast = test_result['per_trend_forecast']
+#
+#             self.per_seasonality_backcast = test_result['per_seasonality_backcast']
+#             self.per_seasonality_forecast = test_result['per_seasonality_forecast']
+#
+#             self.singual_backcast = test_result['singual_backcast']
+#             self.singual_forecast = test_result['singual_forecast']
+#
+#         elif self.config.forecasting_module.name == 'n_beats':
+#             self.stack_per_forecast = np.stack(test_result['stack_per_forecast'])
+#             self.stack_per_backcast = np.stack(test_result['stack_per_backcast'])
+#
+#             self.block_per_backcast = test_result['block_per_backcast']
+#             self.block_per_forecast = test_result['block_per_forecast']
+#
+#     def visualize_val_adj_per_epoch(self):
+#         edge_ = []
+#
+#         for i in range(self.config.train.epoch):
+#             edge_.append(dense_to_sparse(self.train_result['val_adj_matirix'][i])[0].shape[1])
+#
+#         f, axes = plt.subplots(figsize=(10, 5))
+#
+#         axes.plot(edge_, label='Learned Edge')
+#         axes.set_xlabel('Epoch')
+#         axes.set_ylabel('# of Total Edge')
+#         axes.legend()
+#
+#     def visualize_test_adj_matrix(self):
+#         learn_adj = self.test_result['adj_matrix']
+#
+#         f, axes = plt.subplots(figsize=(10, 10))
+#
+#         axes.imshow(learn_adj, cmap='Greys')
+#
+#     def visualize_learning_curve(self):
+#         f, axes = plt.subplots(figsize=(10, 5))
+#
+#         axes.plot(self.train_result['train_loss'], label='Train Loss')
+#         axes.plot(self.train_result['val_loss'], label='Validation Loss')
+#         axes.set_xlabel('Epoch')
+#         axes.set_ylabel('Loss')
+#         axes.legend()
+#
+#     def visualize_node(self, node: int = -2, figure_num: int = 5, save: bool = False, backcast: bool = False):
+#         nrow = figure_num
+#         ncol = 1
+#
+#         input_length = self.config.forecasting_module.backcast_length
+#         output_length = self.config.forecasting_module.forecast_length
+#
+#         x_axis = np.arange(input_length + output_length)
+#
+#         f, axes = plt.subplots(nrows=nrow, ncols=ncol, figsize=(4 * nrow, 50 * ncol), dpi=70)
+#
+#         for ii in range(nrow):
+#             axes[ii].plot(x_axis[:input_length], self.inputs[ii, node, 0], label='Input')
+#
+#             if backcast:
+#                 axes[ii].plot(x_axis[:input_length], self.backcast[ii, node], label='Backcast')
+#
+#             axes[ii].plot(x_axis[input_length:], self.target[ii, node], label='Target')
+#             axes[ii].plot(x_axis[input_length:], self.pred[ii, node], label='Prediction')
+#             axes[ii].legend()
+#
+#         if save:
+#             f.savefig('./total_result.png')
+#
+#     def visualize_stack_output(self, node: int = -2, save: bool = False):
+#         nrow = self.per_trend_backcast.shape[1] + self.singual_backcast.shape[1]
+#         ncol = 2
+#
+#         input_length = self.config.forecasting_module.backcast_length
+#         output_length = self.config.forecasting_module.forecast_length
+#
+#         x_axis = np.arange(input_length + output_length)
+#
+#         if self.config.forecasting_module.name == 'pn_beats':
+#             f, axes = plt.subplots(nrows=nrow, ncols=ncol, figsize=(4 * nrow, 50 * ncol), dpi=70)
