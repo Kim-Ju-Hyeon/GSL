2d8de47f028cc30fca63b40e541d1f732a8c1861
KimJuHyeon <wngus1310@naver.com>
2022-08-22 01:34:04 +0900

    EXP

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -9,13 +9,17 @@ export CUDA_VISIBLE_DEVICES=1
 python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 2 &
 sleep 3
 
-#export CUDA_VISIBLE_DEVICES=2
-#python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 3 --mlp_num 3 &
-#sleep 3
-#
-#export CUDA_VISIBLE_DEVICES=3
-#python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_exchange.yaml --stack_num 3 --mlp_num 3 &
-#sleep 3
+export CUDA_VISIBLE_DEVICES=2
+python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_groud_truth.yaml &
+sleep 3
+python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_random.yaml &
+sleep 3
+
+export CUDA_VISIBLE_DEVICES=3
+python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_no_graph.yaml &
+sleep 3
+python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_gl.yaml &
+sleep 3
 
 export CUDA_VISIBLE_DEVICES=4
 python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 3 &
