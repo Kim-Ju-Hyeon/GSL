3a80f17eabb706e54bf7c4b49f3699c513a8ecf1
KimJuHyeon <wngus1310@naver.com>
2022-08-21 17:29:34 +0900

    METR-LA EXP

--- GSL/exp_rtx90.sh
+++ GSL/exp_rtx90.sh
@@ -3,11 +3,11 @@
 export CUDA_VISIBLE_DEVICES=2
 python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_groud_truth.yaml &
 sleep 3
-python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_no_graph.yaml &
+python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_random.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=3
-python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_random.yaml &
+python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_no_graph.yaml &
 sleep 3
-python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_random2.yaml &
+python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_gl.yaml &
 sleep 3
