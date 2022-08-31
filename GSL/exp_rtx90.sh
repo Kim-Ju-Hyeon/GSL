75138db81baf8f968d9a13c3b40ea14f5352db1b
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-08-31 16:15:24 +0900

    WTH exp

--- GSL/exp_rtx90.sh
+++ GSL/exp_rtx90.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=2
-python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_groud_truth.yaml &
-sleep 3
-python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_random.yaml &
+export CUDA_VISIBLE_DEVICES=0
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 1 &
 sleep 3
 
-export CUDA_VISIBLE_DEVICES=3
-python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_no_graph.yaml &
-sleep 3
-python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_gl.yaml &
+export CUDA_VISIBLE_DEVICES=1
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 3 &
 sleep 3
