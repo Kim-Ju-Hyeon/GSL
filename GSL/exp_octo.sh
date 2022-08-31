be01df0bac703ef65bd4e07e4c4aed4dcfc8e34d
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-08-31 12:23:14 +0900

    exp

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -2,35 +2,35 @@
 
 
 export CUDA_VISIBLE_DEVICES=0
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 1 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ecl.yaml --stack_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 2 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ecl.yaml --stack_num 2 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=2
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 3 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ecl.yaml --stack_num 3 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=3
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 1 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ecl.yaml --stack_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=4
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 2 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ecl.yaml --stack_num 2 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=5
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 3 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ecl.yaml --stack_num 3 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=6
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 1 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ecl.yaml --stack_num 1 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=7
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 2 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_ecl.yaml --stack_num 2 &
 sleep 3
 
 #export CUDA_VISIBLE_DEVICES=2
