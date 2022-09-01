3f7314994800939c4edd7e888241b94ff9361fab
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-09-01 11:20:30 +0900

    WTH exp shell

--- GSL/exp_rtx90.sh
+++ GSL/exp_rtx90.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 export CUDA_VISIBLE_DEVICES=0
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 1 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/WTH/ic_pnbeats_wth_gl.yaml --stack_num 950315 &
+sleep 3
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/WTH/ic_pnbeats_wth_complete_graph.yaml --stack_num 950315 &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ic_pnbeats_wth.yaml --stack_num 3 &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/WTH/ic_pnbeats_wth_gl2.yaml --stack_num 950315 &
+sleep 3
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/WTH/ic_pnbeats_wth_no_graph.yaml --stack_num 950315 &
 sleep 3
