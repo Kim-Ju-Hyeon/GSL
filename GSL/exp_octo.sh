24b1c5ce964679b768dd1ad0ab48ce172f37a0ee
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-09-05 15:47:19 +0900

    exp

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -2,19 +2,19 @@
 
 
 export CUDA_VISIBLE_DEVICES=0
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_96.yaml
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_96.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_192.yaml
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_192.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=2
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_336.yaml
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_336.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=3
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_720.yaml
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_720.yaml &
 sleep 3
 
 #export CUDA_VISIBLE_DEVICES=4
