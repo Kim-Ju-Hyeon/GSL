8cdd26d51cb7624d6d673877fdfceecb8336d1c8
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-09-06 18:47:38 +0900

    ETTm2 EXP

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 export CUDA_VISIBLE_DEVICES=0
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/Exchange/ic_pnbeats_exchange_96.yaml &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ETTm2/ic_pnbeats_ettm2_96.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/Exchange/ic_pnbeats_exchange_192.yaml &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ETTm2/ic_pnbeats_ettm2_192.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=2
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/Exchange/ic_pnbeats_exchange_336.yaml &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ETTm2/ic_pnbeats_ettm2_336.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=3
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/Exchange/ic_pnbeats_exchange_720.yaml &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ETTm2/ic_pnbeats_ettm2_720.yaml &
 sleep 3
 
 #export CUDA_VISIBLE_DEVICES=0
