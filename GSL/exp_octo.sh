830148d1ad7625cabb8594565c72c8fb79785aba
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-09-06 16:45:46 +0900

    shell

--- GSL/exp_octo.sh
+++ GSL/exp_octo.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-
-<<<<<<< HEAD
 export CUDA_VISIBLE_DEVICES=0
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_exchange_96.yaml &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/Exchange/ic_pnbeats_exchange_96.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=1
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_exchange_192.yaml &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/Exchange/ic_pnbeats_exchange_192.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=2
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_exchange_336.yaml &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/Exchange/ic_pnbeats_exchange_336.yaml &
 sleep 3
 
 export CUDA_VISIBLE_DEVICES=3
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_exchange_720.yaml &
+python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/Exchange/ic_pnbeats_exchange_720.yaml &
 sleep 3
-=======
+
 #export CUDA_VISIBLE_DEVICES=0
 #python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_96.yaml &
 #sleep 3
@@ -34,21 +32,21 @@ sleep 3
 #python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_720.yaml &
 #sleep 3
 
-export CUDA_VISIBLE_DEVICES=4
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_96.yaml &
-sleep 3
-
-export CUDA_VISIBLE_DEVICES=5
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_192.yaml &
-sleep 3
-
-export CUDA_VISIBLE_DEVICES=6
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_336.yaml &
-sleep 3
-
-export CUDA_VISIBLE_DEVICES=7
-python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_720.yaml &
-sleep 3
+#export CUDA_VISIBLE_DEVICES=4
+#python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_96.yaml &
+#sleep 3
+#
+#export CUDA_VISIBLE_DEVICES=5
+#python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_192.yaml &
+#sleep 3
+#
+#export CUDA_VISIBLE_DEVICES=6
+#python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_336.yaml &
+#sleep 3
+#
+#export CUDA_VISIBLE_DEVICES=7
+#python3 run_ic_pnbeats_exp.py --conf_file_path ./config/IC_PN_BEATS/ECL/ic_pnbeats_ecl_720.yaml &
+#sleep 3
 
 #export CUDA_VISIBLE_DEVICES=2
 #python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_groud_truth.yaml &
@@ -61,4 +59,3 @@ sleep 3
 #sleep 3
 #python3 grid_search_metr_la_exp.py --conf_file_path ./config/IC_PN_BEATS/METR_LA/ic_pnbeats_metr_la_gl.yaml &
 #sleep 3
->>>>>>> 38a8523e0ddceb2c14b4cb7b1954dbc3354c502d
